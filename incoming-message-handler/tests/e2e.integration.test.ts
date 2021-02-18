84038e92df057a29be70ac150d00a6dff26b4ac7
Samuel Slade <samuel.slade@sladesoftware.co.uk>
2021-02-18 15:30:24 +0000

    Setup mechanism for triggering integration tests

---
+++ incoming-message-handler/tests/e2e.integration.test.ts
@@ -0,0 +1,12 @@
+describe("integration tests", () => {
+  it("should receive a message on the target queue when the message is sent to the AWS SQS queue", () => {
+    // TODO: Subscribe to IBM MQ queue.
+    // TODO: Send a message to SQS.
+    // TODO: Did we get a message?
+    //          Yes - verify the message is correct
+    //          No - timeout and fail
+    // TODO: Can we setup some special logging mechanism and read the logs here?
+
+    expect(true).toBe(true)
+  })
+})
