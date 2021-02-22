a69d243f25f4297fbe9ad16aab9c755d355da924
Samuel Slade <samuel.slade@sladesoftware.co.uk>
2021-02-22 10:05:23 +0000

    Current stage using a dockerfile to spin up infrastructure

--- incoming-message-handler/tests/environment/infrastructure/docker-start.sh
+++ incoming-message-handler/tests/environment/infrastructure/docker-start.sh
@@ -2,6 +2,5 @@
 
 set -e
 
-# Make sure we wait for localstack to be running before we run the setup commands
 ./wait.sh
 ./setup.sh
--- incoming-message-handler/tests/environment/infrastructure/wait.sh
+++ incoming-message-handler/tests/environment/infrastructure/wait.sh
@@ -2,7 +2,16 @@
 
 set -e
 
+elapsed=0
+
 until [ -n "$(curl --silent http://localstack_main:4566 | grep running)" ]; do
   echo "Waiting for local infrastructure to be available..."
   sleep 1
+
+  elapsed=($elapsed+1)
+
+  if [[ $elapsed -eq 30 ]]; then
+    echo "Failed to detect infrastructure"
+    exit 1
+  fi
 done
