5f58445775d52363e24c10289ff0dffabcb7ec6e
Samuel Slade <12849513+sladesamuel@users.noreply.github.com>
2021-02-23 13:27:22 +0000

    Feature/integration testing (#9)
    BICAWS-469: Setup integration testing.
    
    * Setup mechanism for triggering integration tests
    
    * Sending message to SQS queue in integration test
    
    * Partial working state for spinning up local infrastructure
    
    * Current stage using a dockerfile to spin up infrastructure
    
    * Local script to setup infrastructure and streamline integration testing
    
    * Infra setup script is now idempotent
    
    * Local happy path integration test now works with aws-sdk
    
    * Completed integration test for happy path
    
    * Added IbmMqService to tidy up receiving messages from IBM MQ instance
    
    * Moved AWS SQS send message function into helper class for tests
    
    * Fixed lint errors and warnings
    
    * Replaced reflect-metadata with node in tsconfig types compiler options
    
    * Updated README files for the repository and contained modules/projects
    
    * Locked the husky dependency to 4.3.8
    
    * Added readme section relating to steps to complete before raising a pull request
    
    Co-authored-by: Samuel Slade <samuel.slade@sladesoftware.co.uk>

---
+++ incoming-message-handler/tests/IbmMqService.ts
@@ -0,0 +1,61 @@
+import * as https from "https"
+import axios, { AxiosError, AxiosResponse } from "axios"
+import { PromiseResult } from "@handlers/common"
+import { MqConfig } from "../src/types"
+
+const getQueueUrl = (config: MqConfig): string =>
+  `https://${config.MQ_USER}:${config.MQ_PASSWORD}@${config.MQ_HOST}:${config.MQ_PORT}/ibmmq/rest/v1/messaging/qmgr/${config.MQ_QUEUE_MANAGER}/queue/${config.MQ_QUEUE}`
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const sendDeleteRequest = (url: string, httpsAgent: any): Promise<AxiosResponse> =>
+  new Promise<AxiosResponse>((resolve, reject) => {
+    axios
+      .delete(url, {
+        headers: {
+          "ibm-mq-rest-csrf-token": "blank",
+          "Content-Type": "text/plain;charset=utf-8"
+        },
+        httpsAgent
+      })
+      .then((response: AxiosResponse) => resolve(response))
+      .catch((error: AxiosError) => reject(error))
+  })
+
+export default class IbmMqService {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  private httpsAgent: any
+
+  constructor(private config: MqConfig) {
+    this.httpsAgent = new https.Agent({
+      rejectUnauthorized: false
+    })
+  }
+
+  async getMessage(): PromiseResult<string> {
+    const url = `${getQueueUrl(this.config)}/message`
+    const response = await sendDeleteRequest(url, this.httpsAgent)
+    const body = response && response.data && response.data.body
+
+    if (!body) {
+      return new Error("Response received but no content")
+    }
+
+    return body
+  }
+
+  async clearQueue(): PromiseResult<void> {
+    const url = `${getQueueUrl(this.config)}/message`
+    let response
+
+    do {
+      try {
+        // eslint-disable-next-line no-await-in-loop
+        response = await sendDeleteRequest(url, this.httpsAgent)
+      } catch (error) {
+        return error
+      }
+    } while (!response || response.status !== 204)
+
+    return undefined
+  }
+}
---
+++ incoming-message-handler/tests/e2e.integration.test.ts
@@ -0,0 +1,31 @@
+import IncomingMessageSimulator from "./IncomingMessageSimulator"
+import IbmMqService from "./IbmMqService"
+
+jest.setTimeout(30000)
+
+const simulator = new IncomingMessageSimulator("http://localhost:4566")
+
+const mq = new IbmMqService({
+  MQ_HOST: "localhost",
+  MQ_PORT: "10443",
+  MQ_QUEUE_MANAGER: "BR7_QM",
+  MQ_QUEUE: "DEV.QUEUE.1",
+  MQ_USER: "app",
+  MQ_PASSWORD: "passw0rd"
+})
+
+const waitFor = (milliseconds: number): Promise<void> =>
+  new Promise<void>((resolve) => setTimeout(() => resolve(), milliseconds))
+
+describe("integration tests", () => {
+  it("should receive a message on the target queue when the message is sent to the AWS SQS queue", async () => {
+    const expectedMessage = "Hello, World!"
+
+    await mq.clearQueue()
+    await simulator.sendMessage(expectedMessage)
+    await waitFor(3000)
+
+    const actualMessage = await mq.getMessage()
+    expect(actualMessage).toBe(expectedMessage)
+  })
+})
