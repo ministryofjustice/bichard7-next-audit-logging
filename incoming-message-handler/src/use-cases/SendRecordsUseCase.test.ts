35f34daa5ec2d785b068cc4706bfea6de72c5ceb
smradif <69519806+smradif@users.noreply.github.com>
2021-02-24 15:13:24 +0000

    Refactor and implement clean architecture (#10)
    * Refactor and implement clean architecture
    
    * Remove console log
    
    * Fix issues raised from the PR
    
    * Fix issues raised from the PR
    
    * Fix issues raised from the PR
    
    * Throw if any of the Promise.allSettled has failed
    
    * Remove not needed test
    
    Co-authored-by: Saad Radif <saad.radif@madetech.com>

---
+++ incoming-message-handler/src/use-cases/SendRecordsUseCase.test.ts
@@ -0,0 +1,49 @@
+import { SQSRecord } from "aws-lambda"
+import SendRecordsUseCase from "./SendRecordsUseCase"
+import MqGateway from "../gateways/MqGateway"
+
+describe("sendRecords", () => {
+  let gateway: MqGateway
+  let useCase: SendRecordsUseCase
+  let records: SQSRecord[]
+  beforeAll(async () => {
+    records = [
+      {
+        messageId: "1",
+        receiptHandle: "",
+        body: "message 1",
+        attributes: {
+          ApproximateReceiveCount: "",
+          SenderId: "2",
+          SentTimestamp: "",
+          ApproximateFirstReceiveTimestamp: ""
+        },
+        messageAttributes: {},
+        md5OfBody: "md5 Of Body",
+        eventSource: "event source",
+        eventSourceARN: "event Source ARN",
+        awsRegion: "region"
+      }
+    ]
+  })
+  beforeEach(() => {
+    gateway = new MqGateway({})
+    useCase = new SendRecordsUseCase(gateway)
+  })
+
+  it("should handle successful calls", async () => {
+    jest.spyOn(gateway, "execute").mockResolvedValue()
+    const response = await useCase.sendRecords(records)
+    expect(response).toEqual(undefined)
+  })
+
+  it("should handle failed calls", async () => {
+    const error = new Error("Call failed")
+    jest.spyOn(gateway, "execute").mockRejectedValue(error)
+    try {
+      await useCase.sendRecords(records)
+    } catch (e) {
+      expect(e).toEqual(error)
+    }
+  })
+})
