d1615bd01028cf5df1e0d9818a2adcfe2bd413fe
Samuel Slade <samuel.slade@sladesoftware.co.uk>
2021-03-01 10:25:03 +0000

    Finalized cleanup of lambda docker containers for local debugging

--- incoming-message-handler/scripts/stop-debugging.sh
+++ incoming-message-handler/scripts/stop-debugging.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
-echo TODO: Clear down docker.
+# When we spin up the process in the background, it
+# does not detach properly, so manually stop and
+# remove all AWS SAM CLI emulation containers that
+# have been used to host our lambda in debug mode
+docker ps -a |\
+  grep "amazon/aws-sam-cli-emulation-image-nodejs" |\
+  cut -c -12 |\
+  xargs docker stop |\
+  xargs docker rm
