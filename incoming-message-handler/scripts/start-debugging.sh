d7fc05f4f77bbb4b1d356db475dc887421f8b041
Samuel Slade <samuel.slade@sladesoftware.co.uk>
2021-03-09 09:55:48 +0000

    Pass through env vars for SAM lambda debugging

--- incoming-message-handler/scripts/start-debugging.sh
+++ incoming-message-handler/scripts/start-debugging.sh
@@ -4,7 +4,22 @@ echo "Building the debug image..."
 sam build
 
 echo "Starting AWS SAM CLI..."
-sam local invoke IncomingMessageHandler -d 9999 -e scripts/event.json &
+TMPVARS=/tmp/incoming-message-handler.vars
+
+# Note: AWS SAM uses the same environment variables, but the JSON object must be named the
+# same as the lambda resource defined in the SAM template YAML, but we don't want to maintain
+# two files that define the same environment variables.
+cat environment/infrastructure/environment.json | sed -e "s/Variables/IncomingMessageHandler/g" > $TMPVARS
+
+sam local invoke IncomingMessageHandler \
+  -d 9999 \
+  --env-vars $TMPVARS \
+  -e scripts/event.json \
+  --docker-network bichard_audit_logging \
+  -l incoming-message-handler.log \
+  &
 
 echo "Waiting for debugger to attach..."
 sleep 5
+
+rm $TMPVARS
