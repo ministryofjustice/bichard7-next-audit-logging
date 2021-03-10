2407c2fae67243adb65d3aafe76edc84e18b37cb
Saad Radif <saad.radif@madetech.com>
2021-03-10 14:45:41 +0000

    Separate e2e tests from integration tests

--- incoming-message-handler/scripts/test.sh
+++ incoming-message-handler/scripts/test.sh
@@ -9,7 +9,7 @@ case $STYLE in
 
   "unit")
     echo "Running unit tests..."
-    PARAMS+=(--watchAll --testPathIgnorePatterns=integration)
+    PARAMS+=(--watchAll --testPathIgnorePatterns="integration" "e2e")
     ;;
 
   "integration")
@@ -17,10 +17,20 @@ case $STYLE in
     PARAMS+=(--watchAll --testPathPattern=integration)
     ;;
 
+  "e2e")
+    echo "Running e2e tests..."
+    PARAMS+=(--watchAll --testPathPattern=e2e)
+    ;;
+
+  "all")
+    echo "Running all tests..."
+    PARAMS+=(--watchAll)
+    ;;
+
   "ci")
     echo "Running CI tests..."
     JEST_JUNIT_OUTPUT_DIR=./test-results/jest
-    PARAMS+=(--coverage --ci --watchAll false --runInBand --reporters=default --reporters=jest-junit --testPathIgnorePatterns=integration)
+    PARAMS+=(--coverage --ci --watchAll false --runInBand --reporters=default --reporters=jest-junit --testPathIgnorePatterns="integration" "e2e")
     ;;
 esac
 
