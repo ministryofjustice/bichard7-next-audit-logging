1f774c2ffcfe168925fa221f14fc68e3d53377e8
Samuel Slade <samuel.slade@sladesoftware.co.uk>
2021-03-16 15:32:01 +0000

    Moved S3 file retrieval to retrieve from S3 lambda

--- incoming-message-handler/src/retrieveFromS3.ts
+++ incoming-message-handler/src/retrieveFromS3.ts
@@ -1,10 +1,21 @@
+import { isError } from "@handlers/common"
+import { createS3Config } from "./configs"
 import { S3PutObjectEvent, ReceivedMessage } from "./entities"
+import S3Gateway from "./gateways/S3Gateway"
+
+const s3Gateway = new S3Gateway(createS3Config())
 
 export default async function retrieveFromS3(event: S3PutObjectEvent): Promise<ReceivedMessage> {
-  await Promise.resolve()
+  const { bucketName, key } = event.detail.requestParameters
+  const message = await s3Gateway.getItem(bucketName, key)
+
+  if (isError(message)) {
+    throw message
+  }
 
   return {
+    // TODO: Parse ReceivedDate from object key.
     receivedDate: new Date(),
-    messageXml: event.detail.requestParameters.bucketName
+    messageXml: message
   }
 }
