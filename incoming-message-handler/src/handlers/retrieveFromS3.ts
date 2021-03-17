4c360ff7a5ca2983d6339e62d63741b730bacf71
Samuel Slade <samuel.slade@sladesoftware.co.uk>
2021-03-17 11:18:19 +0000

    Read received date from S3 object key

--- incoming-message-handler/src/handlers/retrieveFromS3.ts
+++ incoming-message-handler/src/handlers/retrieveFromS3.ts
@@ -2,20 +2,22 @@ import { isError } from "@handlers/common"
 import { createS3Config } from "src/configs"
 import { S3PutObjectEvent, ReceivedMessage } from "src/entities"
 import S3Gateway from "src/gateways/S3Gateway"
+import readReceivedDateFromS3ObjectKey from "src/utils/readReceivedDateFromS3ObjectKey"
 
 const s3Gateway = new S3Gateway(createS3Config())
 
 export default async function retrieveFromS3(event: S3PutObjectEvent): Promise<ReceivedMessage> {
   const { bucketName, key } = event.detail.requestParameters
-  const message = await s3Gateway.getItem(bucketName, key)
+  const messageXml = await s3Gateway.getItem(bucketName, key)
 
-  if (isError(message)) {
-    throw message
+  if (isError(messageXml)) {
+    throw messageXml
   }
 
+  const receivedDate = readReceivedDateFromS3ObjectKey(key)
+
   return {
-    // TODO: Parse ReceivedDate from object key.
-    receivedDate: new Date(),
-    messageXml: message
+    receivedDate,
+    messageXml
   }
 }
