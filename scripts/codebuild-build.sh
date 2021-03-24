795551c3a7b909b0f7d4c6313ad9df849059dd88
Samuel Slade <samuel.slade@sladesoftware.co.uk>
2021-03-24 14:36:23 +0000

    Refreshed package-lock.json files and added debugging output to CodeBuild scripts

--- scripts/codebuild-build.sh
+++ scripts/codebuild-build.sh
@@ -4,5 +4,9 @@ set -e
 
 echo "Building the Incoming Message Handler..."
 cd ./incoming-message-handler
-npm i
+
+echo "Step 1/2: Install packages"
+npm i -f
+
+echo "Step 2/2: Build output"
 npm run build
--- scripts/codebuild-install.sh
+++ scripts/codebuild-install.sh
@@ -5,3 +5,6 @@ set -e
 # Install NodeJS and the Node Package Manager (npm)
 curl -fsSL https://deb.nodesource.com/setup_15.x | bash -
 apt install -y nodejs
+
+echo "Checking NodeJS version... ${node -v}"
+echo "Checking NPM version... ${npm -v}"
