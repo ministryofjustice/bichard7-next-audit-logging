a21e07c01978dd6e60d3e5754a38978aa04ff8cd
Samuel Slade <samuel.slade@sladesoftware.co.uk>
2021-03-25 16:26:40 +0000

    Fixed integration tests for incoming-message-handler

--- incoming-message-handler/src/gateways/S3Gateway/S3Gateway.integration.test.ts
+++ incoming-message-handler/src/gateways/S3Gateway/S3Gateway.integration.test.ts
@@ -6,7 +6,7 @@ const config: S3Config = {
   S3_URL: "http://localhost:4566",
   S3_REGION: "eu-west-2",
   S3_FORCE_PATH_STYLE: "true",
-  INCOMING_MESSAGE_BUCKET_NAME: "incoming-messages"
+  INCOMING_MESSAGE_BUCKET_NAME: "test-bucket"
 }
 
 const gateway = new TestS3Gateway(config)
@@ -14,6 +14,7 @@ const fileName = "2021/04/09/12/06/123456.xml"
 
 describe("S3Gateway", () => {
   beforeAll(async () => {
+    await gateway.createBucket(config.INCOMING_MESSAGE_BUCKET_NAME)
     await gateway.deleteAll()
   })
 
