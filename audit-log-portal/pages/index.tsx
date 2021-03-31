1c1873bec395348b5514e7270c0e471e7d466cbc
Samuel Slade <samuel.slade@sladesoftware.co.uk>
2021-03-31 09:43:11 +0100

    Removed unused files

---
+++ audit-log-portal/pages/index.tsx
@@ -0,0 +1,15 @@
+import Head from "next/head"
+import Home from "./Home"
+
+const Index = () => (
+  <>
+    <Head>
+      <title>Create Next App</title>
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
+
+    <Home />
+  </>
+)
+
+export default Index
