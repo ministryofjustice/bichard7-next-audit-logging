00a15f5d29d7a141315b84fc45011147cc1fe66d
Samuel Slade <samuel.slade@sladesoftware.co.uk>
2021-03-31 12:08:50 +0100

    Changed text of home page

--- audit-log-portal/pages/Home/Home.tsx
+++ audit-log-portal/pages/Home/Home.tsx
@@ -2,7 +2,7 @@ import styles from "./Home.module.css"
 
 const Home = () => (
   <div className={styles.container}>
-    <h1 className={styles.title}>Home Page</h1>
+    <h1 className={styles.title}>Bichard7 Audit Log Portal</h1>
   </div>
 )
 
