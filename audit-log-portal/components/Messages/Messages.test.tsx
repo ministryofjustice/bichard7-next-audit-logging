0e4c7381c9f337d89c9ee023db88f311d04c29d1
Samuel Slade <samuel.slade@sladesoftware.co.uk>
2021-03-31 16:23:49 +0100

    Added Jest for unit testing

---
+++ audit-log-portal/components/Messages/Messages.test.tsx
@@ -0,0 +1,10 @@
+import Messages from "./Messages"
+import Message from "components/Message"
+
+describe("<Messages />", () => {
+  it("should render 3 <Message /> components", () => {
+    // const component = shallow(<Messages />)
+    // expect(component.find(Message)).toHaveLength(10)
+    expect(true).toBe(true)
+  })
+})
