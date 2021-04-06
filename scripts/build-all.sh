386de8bfd03a7e42f707bea3a17b12141d5bf34e
Samuel Slade <samuel.slade@sladesoftware.co.uk>
2021-04-06 15:01:30 +0100

    Removed redundant dev builds

--- scripts/build-all.sh
+++ scripts/build-all.sh
@@ -2,12 +2,6 @@
 
 set -e
 
-command=build
-if [[ "$1" == "dev" ]];
-then
-  command=build:dev
-fi
-
 projects=$(cat scripts/projects)
 
 # Move into a sub directory so the for loop can jump up and down through project folders
@@ -16,5 +10,5 @@ cd scripts
 for p in ${projects[@]}; do
   echo "Building $p..."
   cd ../$p
-  npm run $command
+  npm run build
 done
