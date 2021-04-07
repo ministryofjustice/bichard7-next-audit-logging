050213632955fd6f801464fc10d6e4ba39979316
Samuel Slade <samuel.slade@sladesoftware.co.uk>
2021-04-07 13:29:04 +0100

    Fixed <Messages /> tests

--- audit-log-portal/src/components/Messages/Messages.test.tsx
+++ audit-log-portal/src/components/Messages/Messages.test.tsx
@@ -2,9 +2,22 @@ import { shallow } from "enzyme"
 import Message from "components/Message"
 import Messages from "./Messages"
 
+const messages = [
+  {
+    messageId: "Message1",
+    caseId: "123",
+    receivedDate: new Date()
+  },
+  {
+    messageId: "Message2",
+    caseId: "456",
+    receivedDate: new Date()
+  }
+]
+
 describe("<Messages />", () => {
-  it("should render 10 <Message /> components", () => {
-    const component = shallow(<Messages />)
-    expect(component.find(Message)).toHaveLength(10)
+  it("should render 2 <Message /> components", () => {
+    const component = shallow(<Messages messages={messages} />)
+    expect(component.find(Message)).toHaveLength(2)
   })
 })
