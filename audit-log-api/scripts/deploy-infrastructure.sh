9ba0b20aed1e728011ba13e127f8afea21e1e9e1
Samuel Slade <samuel.slade@sladesoftware.co.uk>
2021-04-07 15:48:02 +0100

    Local API infra script tidy up

--- audit-log-api/scripts/deploy-infrastructure.sh
+++ audit-log-api/scripts/deploy-infrastructure.sh
@@ -18,7 +18,7 @@ function create_lambda {
   # Configure the lambda with environment variables
   awslocal lambda update-function-configuration \
     --function-name $LAMBDA_NAME \
-    --environment file://$PWD/environment/env-vars.json
+    --environment file://$PWD/scripts/env-vars.json
 }
 
 function create_rest_api {
@@ -32,48 +32,66 @@ function create_rest_api {
     awslocal apigateway create-rest-api \
         --name $API_NAME \
         --region $REGION
+  fi
 
-    API_ID=$(awslocal apigateway get-rest-apis --query "items[?name==\`$API_NAME\`].id" --output text --region $REGION)
+  API_ID=$(awslocal apigateway get-rest-apis \
+    --query "items[?name==\`$API_NAME\`].id" \
+    --output text \
+    --region $REGION \
+  )
 
-    PARENT_RESOURCE_ID=$(awslocal apigateway get-resources --rest-api-id $API_ID --query 'items[?path==`/`].id' --output text --region $REGION)
+  PARENT_RESOURCE_ID=$(awslocal apigateway get-resources \
+    --rest-api-id $API_ID \
+    --query 'items[?path==`/`].id' \
+    --output text \
+    --region $REGION \
+  )
 
-    awslocal apigateway create-resource \
+  # Create the REST endpoint
+  RESOURCE_ID=$(awslocal apigateway get-resources \
+    --rest-api-id $API_ID \
+    --query 'items[?path==`/messages`].id' \
+    --output text \
+    --region $REGION \
+  )
+
+  if [[ -z $RESOURCE_ID ]]; then
+    RESOURCE_ID=$(awslocal apigateway create-resource \
         --region $REGION \
         --rest-api-id $API_ID \
         --parent-id $PARENT_RESOURCE_ID \
-        --path-part "{events}"
-
-    RESOURCE_ID=$(awslocal apigateway get-resources --rest-api-id $API_ID --query 'items[?path==`/{events}`].id' --output text --region $REGION)
-
-    awslocal apigateway put-method \
-        --region $REGION \
-        --rest-api-id $API_ID \
-        --resource-id $RESOURCE_ID \
-        --http-method GET \
-        --request-parameters "method.request.path.events=true" \
-        --authorization-type "NONE"
-
-    awslocal apigateway put-integration \
-        --region $REGION \
-        --rest-api-id $API_ID \
-        --resource-id $RESOURCE_ID \
-        --http-method GET \
-        --type AWS_PROXY \
-        --integration-http-method POST \
-        --uri arn:aws:apigateway:$REGION:lambda:path/2015-03-31/functions/$LAMBDA_ARN/invocations \
-        --passthrough-behavior WHEN_NO_MATCH
-
-    awslocal apigateway create-deployment \
-        --region $REGION \
-        --rest-api-id $API_ID \
-        --stage-name $STAGE
+        --path-part "messages" | jq -r '.id' \
+    )
   fi
 
-  API_ID=$(awslocal apigateway get-rest-apis --query "items[?name==\`$API_NAME\`].id" --output text --region $REGION)
-  
-  ENDPOINT=http://localhost:4566/restapis/$API_ID/$STAGE/_user_request_/messages
+  awslocal apigateway put-method \
+      --region $REGION \
+      --rest-api-id $API_ID \
+      --resource-id $RESOURCE_ID \
+      --http-method GET \
+      --authorization-type "NONE"
 
-  echo "API available at: $ENDPOINT"
+  awslocal apigateway put-integration \
+      --region $REGION \
+      --rest-api-id $API_ID \
+      --resource-id $RESOURCE_ID \
+      --http-method GET \
+      --type AWS \
+      --integration-http-method GET \
+      --uri arn:aws:apigateway:$REGION:lambda:path/2015-03-31/functions/$LAMBDA_ARN/invocations
+
+  awslocal apigateway create-deployment \
+      --region $REGION \
+      --rest-api-id $API_ID \
+      --stage-name $STAGE
+
+  API_ID=$(awslocal apigateway get-rest-apis \
+    --query "items[?name==\`$API_NAME\`].id" \
+    --output text \
+    --region $REGION \
+  )
+
+  echo "API available at: http://localhost:4566/restapis/$API_ID/$STAGE/_user_request_/"
 }
 
 create_lambda "GetMessages" "getMessages.default"
@@ -83,4 +101,4 @@ GET_MESSAGE_API_ARN=$( \
   jq ".[] | map(select(.FunctionName == \"GetMessages\"))" | \
   jq ".[0].FunctionArn" -r)
 
-create_rest_api "GetMessages" $GET_MESSAGE_API_ARN
+create_rest_api "AuditLogApi" $GET_MESSAGE_API_ARN
