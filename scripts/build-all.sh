8808b45ad4d5e0e4308678766a87fdcc33c05394
Samuel Slade <samuel.slade@sladesoftware.co.uk>
2021-04-08 12:52:23 +0100

    Added quotes around the variable substitution

--- scripts/build-all.sh
+++ scripts/build-all.sh
@@ -7,8 +7,8 @@ projects=$(cat scripts/projects)
 # Move into a sub directory so the for loop can jump up and down through project folders
 cd scripts
 
-for p in ${projects[@]}; do
+for p in "${projects[@]}"; do
   echo "Building $p..."
-  cd ../$p
+  cd "../$p"
   npm run build
 done
