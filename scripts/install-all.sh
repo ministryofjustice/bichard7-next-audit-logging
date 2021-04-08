9994daf8c1e1dee16d220b562e45f6be57b50fd9
Samuel Slade <samuel.slade@sladesoftware.co.uk>
2021-04-08 11:58:04 +0100

    Changed loop approach to use IFS and while to read each line from the file

--- scripts/install-all.sh
+++ scripts/install-all.sh
@@ -2,16 +2,14 @@
 
 set -e
 
-projects=$(cat scripts/projects)
-
 echo Installing root dependencies...
 npm i
 
 # Move into a sub directory so the for loop can jump up and down through project folders
 cd scripts
 
-for p in ${projects[@]}; do
-  echo "Installing $p dependencies..."
-  cd ../$p
+while IFS= read -r project; do
+  echo "Installing $project dependencies..."
+  cd ../$project
   npm i
-done
+done < "projects"
