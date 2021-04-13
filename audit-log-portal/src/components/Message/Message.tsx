56c1a1f616f77d45200d3e825612e25c190a6fca
Emad Karamad <emad.karamad@madetech.com>
2021-04-13 09:58:01 +0100

    Reverted Message.tsx formatting changes

--- audit-log-portal/src/components/Message/Message.tsx
+++ audit-log-portal/src/components/Message/Message.tsx
@@ -15,17 +15,27 @@ const Message = ({ message }: Props) => (
   <div className={classNames(styles.container, "row")}>
     <div className={styles.innerContainer}>
       <div className={styles.informationBlock}>
-        <label className={styles.label}>Message Id</label>
-        <p>{message.messageId}</p>
+        <label className={styles.label}>
+          Message Id
+        </label>
+        <p>
+          {message.messageId}
+        </p>
       </div>
 
       <div className={styles.informationBlock}>
-        <label className={styles.label}>Case Number</label>
-        <p>{message.caseId}</p>
+        <label className={styles.label}>
+          Case Number
+        </label>
+        <p>
+          {message.caseId}
+        </p>
       </div>
 
       <div className={styles.informationBlock}>
-        <label className={styles.label}>Received Date</label>
+        <label className={styles.label}>
+          Received Date
+        </label>
         <p>
           <DateTime date={message.receivedDate} />
         </p>
