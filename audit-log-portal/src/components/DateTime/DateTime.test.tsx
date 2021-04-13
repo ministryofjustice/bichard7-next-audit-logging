24cf8be428132084c977604a53db68dfbba55ecd
Emad Karamad <emad.karamad@madetech.com>
2021-04-13 09:52:54 +0100

    Updated DateTime component test

--- audit-log-portal/src/components/DateTime/DateTime.test.tsx
+++ audit-log-portal/src/components/DateTime/DateTime.test.tsx
@@ -6,14 +6,14 @@ describe("<DateTime />", () => {
     const date = new Date("2021-04-01T13:20:30")
     const expectedDateString = "01/04/2021 13:20:30"
     const component = shallow(<DateTime date={date} />)
-    expect(component.html()).toBe(`<time>${expectedDateString}</time>`)
+    expect(component.find("time").text()).toContain(expectedDateString)
   })
 
   it("should render correct date time format when date is type of String", () => {
     const dateString = "2021-04-01T13:20:30"
     const expectedDateString = "01/04/2021 13:20:30"
     const component = shallow(<DateTime date={dateString} />)
-    expect(component.html()).toBe(`<time>${expectedDateString}</time>`)
+    expect(component.find("time").text()).toContain(expectedDateString)
   })
 
   it("should not render when date has no value", () => {
