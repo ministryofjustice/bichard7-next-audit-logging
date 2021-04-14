37d830f5e5afa77fc090c003b9d99bbd207d9466
Samuel Slade <samuel.slade@sladesoftware.co.uk>
2021-04-14 09:27:07 +0100

    Portal host creates .env.local from env vars

--- audit-log-portal/host.js
+++ audit-log-portal/host.js
@@ -2,11 +2,20 @@ const host = require("serverless-http")
 const server = require("restana")()
 const files = require("serve-static")
 const path = require("path")
+const fs = require("fs")
 
 const app = require("next")({
   dev: false
 })
 
+// Create the environment variables file
+const envVars = JSON.stringify({
+  NEXT_PUBLIC_API_URL=process.env.AUDIT_LOG_API_URL
+})
+
+fs.writeFileSync(".env.local", envVars)
+
+// Setup the server and which files to host
 const nextRequesthost = app.getRequestHandler()
 
 server.use(files(path.join(__dirname, ".next")))
