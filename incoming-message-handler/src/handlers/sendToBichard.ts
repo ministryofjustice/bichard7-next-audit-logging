24bdfa6f27ea5fa95626a9676a15326420b203d4
Samuel Slade <samuel.slade@sladesoftware.co.uk>
2021-04-28 10:01:07 +0100

    Debugging log output

--- incoming-message-handler/src/handlers/sendToBichard.ts
+++ incoming-message-handler/src/handlers/sendToBichard.ts
@@ -9,17 +9,23 @@ const gateway = new MqGateway(config)
 const sendMessageUseCase = new SendMessageUseCase(gateway)
 
 export default async function sendToBichard(event: AuditLog): Promise<AuditLog> {
+  console.log(`Sending message ${event.messageId} to Bichard`)
+
   let result: Result<void>
 
   try {
     result = await sendMessageUseCase.send(event.messageXml)
   } catch (error) {
+    console.log("An error occurred when sending the message")
     throw new ApplicationError(`Failed to connect to the URL: ${config.url}`, error)
   }
 
   if (isError(result)) {
+    console.log("Result returned an error")
     throw result
   }
+
+  console.log("Complete!")
 
   return event
 }
--- incoming-message-handler/src/use-cases/SendMessageUseCase.ts
+++ incoming-message-handler/src/use-cases/SendMessageUseCase.ts
@@ -5,6 +5,7 @@ export default class SendMessageUseCase {
   constructor(private gateway: MqGateway) {}
 
   send(messageBody: string): PromiseResult<void> {
+    console.log("SendMessageUseCase.send()")
     return this.gateway.execute(messageBody)
   }
 }
