91bbc57c2d71143932afdff48e256d8b632ab938
Emad Karamad <emad.karamad@madetech.com>
2021-04-29 14:53:28 +0100

    Added TestApi to incoming-message-handler

---
+++ incoming-message-handler/src/e2e/TestApi.ts
@@ -0,0 +1,32 @@
+import axios, { AxiosError } from "axios"
+import { readFileSync } from "fs"
+import { AuditLog, Poller, PollCondition } from "shared"
+
+export default class TestApi {
+  private apiUrl: string
+
+  private getApiUrl(): string {
+    if (this.apiUrl) return this.apiUrl
+
+    const environmentVariablesFilePath = `${process.cwd()}/scripts/env-vars.json`
+    const environmentVariables = JSON.parse(readFileSync(environmentVariablesFilePath, "utf-8")).Variables
+    this.apiUrl = environmentVariables.API_URL.replace("localstack_main", "localhost")
+
+    return this.apiUrl
+  }
+
+  async getMessages(): Promise<AuditLog[]> {
+    return axios
+      .get(`${this.getApiUrl()}/messages`)
+      .then((response) => response.data.messages)
+      .catch((error) => <AxiosError>error)
+  }
+
+  async pollForGetMessages(
+    condition: PollCondition<AuditLog[]> = (messages) => messages.length > 0,
+    timeout = 10000,
+    delay = 300
+  ): Promise<AuditLog[]> {
+    return new Poller(() => this.getMessages()).poll(timeout, delay, condition)
+  }
+}
