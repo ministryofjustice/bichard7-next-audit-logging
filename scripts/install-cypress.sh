794d8c50a3825a14a8274eb979daeae779fe8b5b
Emad Karamad <emad.karamad@madetech.com>
2021-05-05 10:44:04 +0100

    Added scripts/install-cypress.sh file to tidy up Circle CI config

---
+++ scripts/install-cypress.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+# Install Cypress dependencies
+sudo apt-get update
+sudo apt-get install libgtk2.0-0 libgtk-3-0 libgbm-dev libnotify-dev libgconf-2-4 libnss3 libxss1 libasound2 libxtst6 xauth xvfb
+
+# Install Cypress
+cd ./audit-log-portal && npx cypress install && cd -
