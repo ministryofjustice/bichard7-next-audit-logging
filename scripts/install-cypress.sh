c471682465f2032dfe37fc4a87a7e739c6c42d72
Emad Karamad <emad.karamad@madetech.com>
2021-05-05 11:10:46 +0100

    Updated apt-get in install-cypress.sh script to auto accept

--- scripts/install-cypress.sh
+++ scripts/install-cypress.sh
@@ -2,7 +2,7 @@
 
 # Install Cypress dependencies
 sudo apt-get update
-sudo apt-get install libgtk2.0-0 libgtk-3-0 libgbm-dev libnotify-dev libgconf-2-4 libnss3 libxss1 libasound2 libxtst6 xauth xvfb
+sudo apt-get install libgtk2.0-0 libgtk-3-0 libgbm-dev libnotify-dev libgconf-2-4 libnss3 libxss1 libasound2 libxtst6 xauth xvfb -y
 
 # Install Cypress
 cd ./audit-log-portal && npx cypress install && cd -
