e7cb8caa126b6e4fa66c031f7e5052c8f5b9b2d1
Emad Karamad <emad.karamad@madetech.com>
2021-05-12 16:26:52 +0100

    Refactored PersistMessageUseCase to pass the api url

--- incoming-message-handler/src/use-cases/PersistMessageUseCase.test.ts
+++ incoming-message-handler/src/use-cases/PersistMessageUseCase.test.ts
@@ -1,15 +1,17 @@
 import axios, { AxiosError } from "axios"
 import { isError, AuditLog } from "shared"
-import persistMessage from "./persistMessage"
+import PersistMessageUseCase from "./PersistMessageUseCase"
 
 const message = new AuditLog("id", new Date(), "XML")
 message.caseId = "123"
+
+const useCase = new PersistMessageUseCase("http://localhost")
 
 describe("persistMessage", () => {
   it("should return true when server responds with 201 status code", async () => {
     jest.spyOn(axios, "post").mockResolvedValue({ status: 201 })
 
-    const result = await persistMessage(message)
+    const result = await useCase.persist(message)
 
     expect(isError(result)).toBe(false)
     expect(result).toBeNull()
@@ -18,7 +20,7 @@ describe("persistMessage", () => {
   it("should fail when server does not responds with 201 status code", async () => {
     jest.spyOn(axios, "post").mockResolvedValue({ status: 400 })
 
-    const result = await persistMessage(message)
+    const result = await useCase.persist(message)
 
     expect(isError(result)).toBe(true)
     expect((<Error>result).message).toContain("400")
@@ -28,7 +30,7 @@ describe("persistMessage", () => {
     const expectedError = <AxiosError>new Error("An unknown error")
     jest.spyOn(axios, "post").mockRejectedValue(expectedError)
 
-    const result = await persistMessage(message)
+    const result = await useCase.persist(message)
 
     expect(isError(result)).toBe(true)
     expect(<Error>result).toBe(expectedError)
