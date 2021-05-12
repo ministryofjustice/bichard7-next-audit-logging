fc0c52d5339411f77f70ec4eeaf2622f67118a54
Emad Karamad <emad.karamad@madetech.com>
2021-05-12 16:21:01 +0100

    Refactored CreateSentToBichardEventUseCase to pass the api url

--- incoming-message-handler/src/handlers/recordSentToBichardEvent.ts
+++ incoming-message-handler/src/handlers/recordSentToBichardEvent.ts
@@ -1,8 +1,10 @@
 import { isError, AuditLog } from "shared"
-import createSentToBichardEvent from "src/use-cases/createSentToBichardEvent"
+import CreateSentToBichardEventUseCase from "src/use-cases/CreateSentToBichardEventUseCase"
+
+const createSentToBichardEventUseCase = new CreateSentToBichardEventUseCase(process.env.API_URL)
 
 export default async function recordSentToBichardEvent(message: AuditLog): Promise<AuditLog> {
-  const result = await createSentToBichardEvent(message)
+  const result = await createSentToBichardEventUseCase.create(message)
 
   if (isError(result)) {
     throw result
--- incoming-message-handler/src/use-cases/CreateSentToBichardEventUseCase.test.ts
+++ incoming-message-handler/src/use-cases/CreateSentToBichardEventUseCase.test.ts
@@ -1,14 +1,15 @@
 import axios, { AxiosError } from "axios"
 import { AuditLog, isError } from "shared"
-import createSentToBichardEvent from "./createSentToBichardEvent"
+import CreateSentToBichardEventUseCase from "./CreateSentToBichardEventUseCase"
 
 const message = new AuditLog("b5edf595-16a9-450f-a52b-40628cd58c29", new Date(), "<XML></XML>")
+const useCase = new CreateSentToBichardEventUseCase("http://localhost")
 
 describe("createSentToBichardEvent()", () => {
   it("should return Created http status code when message exists", async () => {
     jest.spyOn(axios, "post").mockResolvedValue({ status: 201 })
 
-    const result = await createSentToBichardEvent(message)
+    const result = await useCase.create(message)
 
     expect(isError(result)).toBe(false)
   })
@@ -16,7 +17,7 @@ describe("createSentToBichardEvent()", () => {
   it("should fail when message does not exist", async () => {
     jest.spyOn(axios, "post").mockResolvedValue({ status: 404 })
 
-    const result = await createSentToBichardEvent(message)
+    const result = await useCase.create(message)
 
     expect(isError(result)).toBe(true)
     expect((<Error>result).message).toContain("404")
@@ -26,7 +27,7 @@ describe("createSentToBichardEvent()", () => {
     const expectedError = <AxiosError>new Error("An unknown error")
     jest.spyOn(axios, "post").mockRejectedValue(expectedError)
 
-    const result = await createSentToBichardEvent(message)
+    const result = await useCase.create(message)
 
     expect(isError(result)).toBe(true)
     expect(<Error>result).toBe(expectedError)
