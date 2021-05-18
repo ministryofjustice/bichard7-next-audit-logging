4a4c9179cb6668911a79f549ee3164703d26c18c
Emad Karamad <emad.karamad@madetech.com>
2021-05-18 12:37:55 +0100

    Updated test.sh to quit after running tests when running in CI pipeline

--- scripts/test.sh
+++ scripts/test.sh
@@ -5,33 +5,38 @@ set -e
 STYLE=$1
 PARAMS=()
 
+if [[ -z "$TEST_ENVIRONMENT" ]] || [ "$TEST_ENVIRONMENT" != "ci" ]; then
+  PARAMS+=(--watchAll)
+else
+  PARAMS+=(--ci --detectOpenHandles)
+fi
+
 case $STYLE in
 
   "unit")
     echo "Running unit tests..."
-    PARAMS+=(--watchAll --testPathIgnorePatterns="integration" "e2e")
+    PARAMS+=(--testPathIgnorePatterns="integration" "e2e")
     ;;
 
   "integration")
     echo "Running integration tests..."
-    PARAMS+=(--watchAll --testPathPattern=integration)
+    PARAMS+=(--testPathPattern=integration)
     ;;
 
   "e2e")
     echo "Running e2e tests..."
-    PARAMS+=(--watchAll --testPathPattern=e2e)
+    PARAMS+=(--testPathPattern=e2e)
     ;;
 
   "ci")
     echo "Running CI tests..."
     export JEST_JUNIT_OUTPUT_DIR=./test-results/jest
-    PARAMS+=(--coverage --ci --watchAll false --runInBand --reporters=default --reporters=jest-junit --testPathIgnorePatterns="integration" "e2e")
+    PARAMS+=(--coverage --ci false --runInBand --reporters=default --reporters=jest-junit --testPathIgnorePatterns="integration" "e2e")
     ;;
 
     *)
     echo "Running all tests..."
-    PARAMS+=(--watchAll)
     ;;
 esac
 
-jest "${PARAMS[@]}" --testPathPattern=$TEST_PATH_PATTERN --testPathIgnorePatterns="dist" "build"
+jest "${PARAMS[@]}" --testPathPattern=$TEST_PATH_PATTERN --testPathIgnorePatterns="dist" "build" "cypress"
