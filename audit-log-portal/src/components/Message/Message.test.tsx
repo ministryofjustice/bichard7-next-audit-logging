7f551646a4a7586a6785671b0cdfa4581d83db2b
Emad Karamad <emad.karamad@madetech.com>
2021-05-25 10:19:51 +0100

    Added MockDate to mock current date and fix the issue with getDaysOld() in Message.test.tsx

--- audit-log-portal/src/components/Message/Message.test.tsx
+++ audit-log-portal/src/components/Message/Message.test.tsx
@@ -1,11 +1,16 @@
 import { render } from "testing/render"
 import { AuditLog } from "shared"
+import MockDate from "mockdate"
 import Message from "./Message"
 
 test("matches snapshot", () => {
+  MockDate.set(new Date("2021-05-24T10:20:30").getTime())
+
   const message = new AuditLog("ExternalCorrelationId", new Date("2021-05-01T10:25:53"), "XML")
 
   const { container } = render(<Message message={message} />)
 
   expect(container).toMatchSnapshot()
+
+  MockDate.reset()
 })
