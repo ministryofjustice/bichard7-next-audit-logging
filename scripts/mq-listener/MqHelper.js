5efb4e231d09ba707540d5f451a2c4e164fc158a
Emad Karamad <emad.karamad@madetech.com>
2021-05-26 12:30:52 +0100

    Fixed issue with failing to process messages after the first one

--- scripts/mq-listener/MqHelper.js
+++ scripts/mq-listener/MqHelper.js
@@ -35,14 +35,25 @@ class MqHelper {
   async subscribe(queueName, action) {
     this.connect()
     const headers = {
-      destination: `/queue/${queueName}`,
-      ack: "auto"
+      destination: `/queue/${queueName}`
+    }
+
+    const ack = (message) => {
+      this.channel.ack(message)
+      this.channel.close()
+      process.exit()
+    }
+
+    const nack = (message) => {
+      this.channel.nack(message)
+      this.channel.close()
+      process.exit()
     }
 
     return this.channel.subscribe(headers, (error, message) =>
       action(error, message, {
-        ack: () => this.channel.ack(message),
-        nack: () => this.channel.nack(message)
+        ack: () => ack(message),
+        nack: () => nack(message)
       })
     )
   }
