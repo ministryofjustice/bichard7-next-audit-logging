d35e10b2231650987a52e41ff406406cbf7e758a
Samuel Slade <samuel.slade@sladesoftware.co.uk>
2021-05-26 11:14:34 +0100

    Change getCategoryIcon tests to use the test.each approach

--- audit-log-portal/src/components/Event/getCategoryIcon.test.tsx
+++ audit-log-portal/src/components/Event/getCategoryIcon.test.tsx
@@ -1,22 +1,19 @@
+import { EventCategory } from "shared"
 import ErrorIcon from "icons/ErrorIcon"
 import WarningIcon from "icons/WarningIcon"
 import InformationIcon from "icons/InformationIcon"
 import getCategoryIcon from "./getCategoryIcon"
 
-test("returns <ErrorIcon /> when category is error", () => {
-  const Component = getCategoryIcon("error")
+interface TestInput {
+  category: EventCategory
+  expectedIcon: () => JSX.Element
+}
 
-  expect(Component).toBe(ErrorIcon)
-})
-
-test("returns <WarningIcon /> when category is warning", () => {
-  const Component = getCategoryIcon("warning")
-
-  expect(Component).toBe(WarningIcon)
-})
-
-test("returns <InformationIcon /> when category is information", () => {
-  const Component = getCategoryIcon("information")
-
-  expect(Component).toBe(InformationIcon)
+test.each<TestInput>([
+  { category: "error", expectedIcon: ErrorIcon },
+  { category: "warning", expectedIcon: WarningIcon },
+  { category: "information", expectedIcon: InformationIcon }
+])("returns <$expected/> when category is $category", ({ category, expectedIcon }) => {
+  const Component = getCategoryIcon(category)
+  expect(Component).toBe(expectedIcon)
 })
