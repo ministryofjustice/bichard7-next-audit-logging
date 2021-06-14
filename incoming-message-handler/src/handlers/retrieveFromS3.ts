ad3c44bf2ed8ea10dfa897db700ee700963f496b
Emad Karamad <emad.karamad@madetech.com>
2021-06-14 12:19:39 +0100

    Updated retrieveFromS3 function to validate the s3 key and return validation result if failed

--- incoming-message-handler/src/handlers/retrieveFromS3.ts
+++ incoming-message-handler/src/handlers/retrieveFromS3.ts
@@ -2,12 +2,23 @@ import { isError } from "shared"
 import { createS3Config } from "src/configs"
 import { S3PutObjectEvent, ReceivedMessage } from "src/entities"
 import S3Gateway from "src/gateways/S3Gateway"
+import validateS3Key, { ValidateS3KeyResult } from "src/use-cases/validateS3Key"
 import readReceivedDateFromS3ObjectKey from "src/utils/readReceivedDateFromS3ObjectKey"
+
+interface ValidationResult {
+  validationResult: ValidateS3KeyResult
+}
 
 const s3Gateway = new S3Gateway(createS3Config())
 
-export default async function retrieveFromS3(event: S3PutObjectEvent): Promise<ReceivedMessage> {
+export default async function retrieveFromS3(event: S3PutObjectEvent): Promise<ReceivedMessage | ValidationResult> {
   const { bucketName, key } = event.detail.requestParameters
+
+  const validationResult = validateS3Key(key)
+  if (!validationResult.isValid) {
+    return { validationResult }
+  }
+
   const messageXml = await s3Gateway.getItem(bucketName, key)
 
   if (isError(messageXml)) {
