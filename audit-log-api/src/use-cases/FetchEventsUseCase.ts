25e9a0847cc2c84bac1cff33a72e3471f49891cd
Emad Karamad <emad.karamad@madetech.com>
2021-06-21 11:38:17 +0100

    Added FetchEventsUseCase to get the events of a message

---
+++ audit-log-api/src/use-cases/FetchEventsUseCase.ts
@@ -0,0 +1,9 @@
+import { PromiseResult, AuditLogDynamoGateway, AuditLogEvent } from "shared"
+
+export default class FetchEventsUseCase {
+  constructor(private readonly gateway: AuditLogDynamoGateway) {}
+
+  get(messageId: string): PromiseResult<AuditLogEvent[]> {
+    return this.gateway.fetchEvents(messageId)
+  }
+}
