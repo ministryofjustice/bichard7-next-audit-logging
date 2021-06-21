a566099f84a6010797798f6dcd867835fad04d97
Samuel Slade <12849513+sladesamuel@users.noreply.github.com>
2021-06-21 09:32:08 +0100

    Hotfix/fix events portal BICAWS-887 (#91)
    * Fixed retrieval of events in portal
    
    * Fixed display of error messages from API on portal
    
    * Return zero messages when no match on external correlation Id
    
    Co-authored-by: Samuel Slade <samuel.slade@sladesoftware.co.uk>

--- audit-log-portal/src/pages/api/messages/index.ts
+++ audit-log-portal/src/pages/api/messages/index.ts
@@ -8,13 +8,18 @@ type Data = {
   messages: AuditLog[]
 }
 
-export default async (request: NextApiRequest, response: NextApiResponse<Data>) => {
+export default async (request: NextApiRequest, response: NextApiResponse<Data | string>) => {
   const params = convertObjectToURLSearchParams(request.query)
   const baseUrl = `${config.apiUrl}/messages`
   const url = combineUrlAndQueryString(baseUrl, params.toString())
 
   const fetchResponse = await fetch(url)
-  const messages = await fetchResponse.json()
 
-  response.status(200).json({ messages })
+  if (fetchResponse.status !== 200) {
+    const errorMessage = await fetchResponse.text()
+    response.status(fetchResponse.status).send(errorMessage)
+  } else {
+    const messages = await fetchResponse.json()
+    response.status(200).json({ messages })
+  }
 }
