ef2c61ba64a01b156cf47eae0ac688c96fed724b
Emad Karamad <emad.karamad@madetech.com>
2021-07-02 13:52:12 +0100

    Fixed issue with AuditLogStatus import path

--- audit-log-portal/src/components/Message/getStatusIcon.ts
+++ audit-log-portal/src/components/Message/getStatusIcon.ts
@@ -2,7 +2,7 @@ import { ComponentType } from "react"
 import ErrorIcon from "icons/ErrorIcon"
 import SuccessIcon from "icons/SuccessIcon"
 import ProcessingIcon from "icons/ProcessingIcon"
-import { AuditLogStatus } from "shared"
+import AuditLogStatus from "shared/dist/utils/AuditLogStatus"
 
 export default function getStatusIcon(status: string): ComponentType {
   switch (status) {
