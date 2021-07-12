339b25bead11232bbec04f78d6d4b9d9bbdb8a10
Samuel Slade <samuel.slade@sladesoftware.co.uk>
2021-07-12 13:01:12 +0100

    Created testing library under @bichard

---
+++ src/@bichard/testing/src/invokeFunction.ts
@@ -0,0 +1,33 @@
+import { Lambda } from "aws-sdk"
+import { InvocationRequest, InvocationResponse } from "aws-sdk/clients/lambda"
+import { PromiseResult } from "shared"
+
+interface InvocationOptions {
+  endpoint?: string
+  region?: string
+  accessKeyId?: string
+  secretAccessKey?: string
+}
+
+export default async <TPayload, TResponse>(
+  functionName: string,
+  payload: TPayload,
+  options?: InvocationOptions
+): PromiseResult<TResponse> => {
+  const lambda = new Lambda({
+    endpoint: options?.endpoint || "http://localhost:4566",
+    region: options?.region || "us-east-1",
+    credentials: {
+      accessKeyId: options?.accessKeyId || "test",
+      secretAccessKey: options?.secretAccessKey || "test"
+    }
+  })
+
+  const params = <InvocationRequest>{
+    FunctionName: functionName,
+    Payload: JSON.stringify(payload)
+  }
+
+  const response = <InvocationResponse>await lambda.invoke(params).promise()
+  return <TResponse>JSON.parse(<string>response.Payload)
+}
