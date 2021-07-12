fcfb2285f1fefd7ee9e911fe76d6114b179186db
Samuel Slade <samuel.slade@sladesoftware.co.uk>
2021-07-12 11:07:46 +0100

    Implemented logic to create audit log event in Record Event lambda

--- src/lambdas/record-event/index.ts
+++ src/lambdas/record-event/index.ts
@@ -1,3 +1,22 @@
-export default async (): Promise<void> => {
-  await Promise.resolve()
+import { AuditLogEvent, isError } from "shared"
+import AuditLogApiGateway from "./AuditLogApiGateway"
+
+interface RecordEventInput {
+  messageId: string
+  event: AuditLogEvent
+  s3Path: string
+}
+
+const apiUrl = process.env.API_URL
+if (!apiUrl) {
+  throw new Error("API_URL environment variable is not set")
+}
+
+const api = new AuditLogApiGateway(apiUrl)
+
+export default async (input: RecordEventInput): Promise<void> => {
+  const result = await api.createAuditLogEvent(input.messageId, input.event)
+  if (isError(result)) {
+    throw result
+  }
 }
