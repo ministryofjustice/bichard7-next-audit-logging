120d1a135341e0363a2bfd5c44516b828f574ef1
Samuel Slade <samuel.slade@sladesoftware.co.uk>
2021-07-12 11:37:35 +0100

    Parse messageId as part of translation

--- src/lambdas/translate-event/translators/GeneralEventTranslator.ts
+++ src/lambdas/translate-event/translators/GeneralEventTranslator.ts
@@ -1,10 +1,11 @@
-import type { PromiseResult, AuditLogEvent } from "shared"
+import type { PromiseResult } from "shared"
 import { decodeBase64, parseXml } from "shared"
 import type GeneralEventLogItem from "types/GeneralEventLogItem"
+import type TranslationResult from "./TranslationResult"
 import type Translator from "./Translator"
 import transformEventDetails from "./transformEventDetails"
 
-const GeneralEventTranslator: Translator = async (messageData: string): PromiseResult<AuditLogEvent> => {
+const GeneralEventTranslator: Translator = async (messageData: string): PromiseResult<TranslationResult> => {
   // General events are in base64 encoded XML
   const xml = decodeBase64(messageData)
   const logItem = await parseXml<GeneralEventLogItem>(xml)
@@ -13,7 +14,11 @@ const GeneralEventTranslator: Translator = async (messageData: string): PromiseR
     return new Error("Failed to parse the General Event")
   }
 
-  return transformEventDetails(logItem.logEvent)
+  const event = transformEventDetails(logItem.logEvent)
+  return {
+    messageId: logItem.logEvent.correlationID,
+    event
+  }
 }
 
 export default GeneralEventTranslator
