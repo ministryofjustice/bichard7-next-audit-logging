0746cd09c9cc96bd4e90769a15d07bb191bc64bb
Samuel Slade <samuel.slade@sladesoftware.co.uk>
2021-07-13 16:06:12 +0100

    Moved jest extension declaration to index file

--- src/@bichard/testing/src/jest/toNotBeError/index.ts
+++ src/@bichard/testing/src/jest/toNotBeError/index.ts
@@ -1,6 +1,16 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable @typescript-eslint/no-namespace */
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import type { Result } from "shared"
 import { isError } from "shared"
+
+declare global {
+  namespace jest {
+    interface Matchers<R> {
+      toNotBeError(): any
+    }
+  }
+}
 
 expect.extend({
   toNotBeError(received: Result<any>): any {
