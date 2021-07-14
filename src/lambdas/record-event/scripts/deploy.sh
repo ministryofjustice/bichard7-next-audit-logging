911823daca222fb7df5626e968f405d6d5d7b15b
Samuel Slade <samuel.slade@sladesoftware.co.uk>
2021-07-14 11:06:55 +0100

    Added e2de test for Record Event lambda

--- src/lambdas/record-event/scripts/deploy.sh
+++ src/lambdas/record-event/scripts/deploy.sh
@@ -9,6 +9,6 @@ API_URL=$(get_audit_log_api_url "localstack_main")
 ENV_VARS=/tmp/env-vars.json
 
 # Process the template env-vars file with the API URL
-cat "$PWD/scripts/env-vars.json.tpl" | sed "s/\$API_URL/$API_URL/g" > "$ENV_VARS"
+cat "$PWD/scripts/env-vars.json.tpl" | sed "s,{API_URL},$API_URL,g" > "$ENV_VARS"
 
 create_lambda "record-event" "recordEvent.default" "$ENV_VARS"
--- src/lambdas/record-event/scripts/env-vars.json.tpl
+++ src/lambdas/record-event/scripts/env-vars.json.tpl
@@ -1,7 +1,5 @@
 {
   "Variables": {
-    "AWS_URL": "http://localstack_main:4566",
-    "AWS_REGION": "us-east-1",
-    "API_URL": "$API_URL"
+    "API_URL": "{API_URL}"
   }
 }
