b2f797f7179b38b53bd9e362c6a7e5b56e52ab92
Samuel Slade <samuel.slade@sladesoftware.co.uk>
2021-07-15 11:15:42 +0100

    Setup deployment for Message Receiver lambda

--- src/lambdas/message-receiver/scripts/deploy.sh
+++ src/lambdas/message-receiver/scripts/deploy.sh
@@ -4,4 +4,13 @@ set -e
 
 source "$PWD/../../../environment/create-lambda.sh"
 
+# Create a dummy step function that we can execute in the tests
+STEP_FUNCTION_NAME=DummyMessageReceiverStepFunction
+if [[ -z $(awslocal stepfunctions list-state-machines | grep "$STEP_FUNCTION_NAME") ]]; then
+  awslocal stepfunctions create-state-machine \
+    --definition "file://$PWD/scripts/dummy-state-machine.json" \
+    --name "$STEP_FUNCTION_NAME" \
+    --role-arn "arn:aws:iam::012345678901:role/DummyRole"
+fi
+
 create_lambda "message-receiver" "messageReceiver.default" "$PWD/scripts/env-vars.json"
