a934788a46f99fe29d7026a80526ff5e28611df3
Emad Karamad <emad.karamad@madetech.com>
2021-07-15 16:49:35 +0100

    Added IndexSearcher

---
+++ shared/src/DynamoGateway/IndexSearcher.integration.test.ts
@@ -0,0 +1,105 @@
+import type DynamoDbConfig from "./DynamoDbConfig"
+import IndexSearcher from "./IndexSearcher"
+import TestDynamoGateway from "./TestDynamoGateway"
+
+interface TestRecord {
+  id: string
+  someOtherValue: string
+  someOtherValue2: string
+}
+
+const config: DynamoDbConfig = {
+  DYNAMO_URL: "http://localhost:4566",
+  DYNAMO_REGION: "us-east-1",
+  AUDIT_LOG_TABLE_NAME: "SearcherTesting"
+}
+
+const gateway = new TestDynamoGateway(config)
+const partitionKey = "id"
+const indexName = "someOtherValueSecondaryIndex"
+const hashKey = "someOtherValue"
+const hashKeyValue = "Dummy value"
+const rangeKey = "someOtherValue2"
+
+beforeAll(async () => {
+  const options = {
+    keyName: partitionKey,
+    sortKey: "someOtherValue",
+    secondaryIndexes: [
+      {
+        name: indexName,
+        hashKey,
+        rangeKey
+      }
+    ],
+    skipIfExists: true
+  }
+
+  await gateway.createTable(config.AUDIT_LOG_TABLE_NAME, options)
+
+  await gateway.deleteAll(config.AUDIT_LOG_TABLE_NAME, "id")
+
+  await Promise.allSettled(
+    [...Array(15).keys()].map(async (i: number) => {
+      const index = i + 10
+      const record = {
+        id: `Record ${index}`,
+        someOtherValue: hashKeyValue,
+        someOtherValue2: `Value ${index}`
+      }
+
+      await gateway.insertOne(config.AUDIT_LOG_TABLE_NAME, record, "id")
+    })
+  )
+})
+
+it("should return first 3 records when last record is not provided in pagination", async () => {
+  const result = await new IndexSearcher<TestRecord[]>(gateway, config.AUDIT_LOG_TABLE_NAME, partitionKey)
+    .useIndex(indexName)
+    .setIndexKeys(hashKey, hashKeyValue, rangeKey)
+    .paginate(3)
+    .execute()
+
+  expect(result).toBeDefined()
+  const records = result as TestRecord[]
+  expect(records).toHaveLength(3)
+  expect(records[0].id).toBe("Record 24")
+  expect(records[1].id).toBe("Record 23")
+  expect(records[2].id).toBe("Record 22")
+})
+
+it("should return last 5 records when last record is provided in pagination", async () => {
+  const lastRecord: TestRecord = {
+    id: `Record 13`,
+    someOtherValue: hashKeyValue,
+    someOtherValue2: `Value 13`
+  }
+
+  const result = await new IndexSearcher<TestRecord[]>(gateway, config.AUDIT_LOG_TABLE_NAME, partitionKey)
+    .useIndex(indexName)
+    .setIndexKeys(hashKey, hashKeyValue, rangeKey)
+    .paginate(10, lastRecord)
+    .execute()
+
+  expect(result).toBeDefined()
+  const records = result as TestRecord[]
+  expect(records).toHaveLength(3)
+  expect(records[0].id).toBe("Record 12")
+  expect(records[1].id).toBe("Record 11")
+  expect(records[2].id).toBe("Record 10")
+})
+
+it("should return records in ascending order when pagination is set to order ascending", async () => {
+  const result = await new IndexSearcher<TestRecord[]>(gateway, config.AUDIT_LOG_TABLE_NAME, partitionKey)
+    .useIndex(indexName)
+    .setIndexKeys(hashKey, hashKeyValue, rangeKey)
+    .paginate(3, null, true)
+    .execute()
+
+  expect(result).toBeDefined()
+  const records = result as TestRecord[]
+  expect(records).toHaveLength(3)
+  expect(records[0].id).toBe("Record 10")
+  expect(records[1].id).toBe("Record 11")
+  expect(records[2].id).toBe("Record 12")
+})
---
+++ shared/src/DynamoGateway/IndexSearcher.ts
@@ -0,0 +1,79 @@
+import type { KeyValuePair, PromiseResult } from "../types"
+import { isError } from "../types"
+import type DynamoGateway from "./DynamoGateway"
+import type FetchByIndexOptions from "./FetchByIndexOptions"
+import type Pagination from "./Pagination"
+
+export default class IndexSearcher<TResult> {
+  constructor(private gateway: DynamoGateway, private tableName: string, private partitionKey: string) {}
+
+  private indexName: string
+
+  private hashKey: string
+
+  private hashKeyValue: unknown
+
+  private rangeKey?: string
+
+  private limit = 10
+
+  private lastItemForPagination?: KeyValuePair<string, unknown>
+
+  private isAscendingOrder: boolean
+
+  useIndex(indexName: string): IndexSearcher<TResult> {
+    this.indexName = indexName
+    return this
+  }
+
+  setIndexKeys(hashKey: string, hashKeyValue: unknown, rangeKey?: string): IndexSearcher<TResult> {
+    this.hashKey = hashKey
+    this.hashKeyValue = hashKeyValue
+    this.rangeKey = rangeKey
+    return this
+  }
+
+  paginate(
+    limit: number,
+    lastItemForPagination?: unknown | KeyValuePair<string, unknown>,
+    isAscendingOrder = false
+  ): IndexSearcher<TResult> {
+    this.limit = limit
+    this.lastItemForPagination = lastItemForPagination as KeyValuePair<string, unknown>
+    this.isAscendingOrder = isAscendingOrder
+    return this
+  }
+
+  async execute(): PromiseResult<TResult | undefined> {
+    const pagination: Pagination = {
+      limit: this.limit
+    }
+
+    if (this.lastItemForPagination) {
+      pagination.lastItemKey = {
+        [this.partitionKey]: this.lastItemForPagination[this.partitionKey],
+        [this.hashKey]: this.lastItemForPagination[this.hashKey]
+      }
+
+      if (this.rangeKey) {
+        pagination.lastItemKey[this.rangeKey] = this.lastItemForPagination[this.rangeKey]
+      }
+    }
+
+    const options: FetchByIndexOptions = {
+      indexName: this.indexName,
+      attributeName: this.hashKey,
+      attributeValue: this.hashKeyValue,
+      isAscendingOrder: this.isAscendingOrder,
+      pagination
+    }
+
+    const fetchResult = await this.gateway.fetchByIndex(this.tableName, options)
+
+    if (isError(fetchResult)) {
+      return fetchResult
+    }
+
+    return <TResult | undefined>fetchResult.Items
+  }
+}
