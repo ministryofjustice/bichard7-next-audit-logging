846eb9c1254f9e7613af9112658d1a46f9fc7e01
Emad Karamad <emad.karamad@madetech.com>
2021-07-16 14:27:20 +0100

    Feature/bicaws 915 dynamodb index searcher (#112)
    * Added IndexSearcher
    
    * Updated AwsAuditLogDynamoGateway to use IndexSearcher
    
    * Fixed test case description
    
    * Updated IndexSearcher to validate lastItemForPagination
    
    * Updated IndexSearcher tests to expect different number of records
    
    * Extracted LastItemKey logic in IndexSearcher
    
    * Updated validateLastItemForPagination function return type
    
    * Fixed lint issues after merging branches

---
+++ shared/src/DynamoGateway/IndexSearcher.ts
@@ -0,0 +1,121 @@
+/* eslint-disable no-prototype-builtins */
+import type { DocumentClient } from "aws-sdk/clients/dynamodb"
+import type { KeyValuePair, PromiseResult, Result } from "../types"
+import { isError } from "../types"
+import type DynamoGateway from "./DynamoGateway"
+import type FetchByIndexOptions from "./FetchByIndexOptions"
+import type Pagination from "./Pagination"
+
+export default class IndexSearcher<TResult> {
+  constructor(private gateway: DynamoGateway, private tableName: string, private partitionKey: string) {}
+
+  private indexName: string
+
+  private hashKey: string
+
+  private hashKeyValue: unknown
+
+  private rangeKey?: string
+
+  private limit = 10
+
+  private lastItemForPagination?: KeyValuePair<string, unknown>
+
+  private isAscendingOrder: boolean
+
+  private validateLastItemForPagination(): Result<void> {
+    if (!this.lastItemForPagination) {
+      return undefined
+    }
+
+    if (!this.lastItemForPagination.hasOwnProperty(this.partitionKey)) {
+      return new Error(`lastItemForPagination does not contain '${this.partitionKey}' field`)
+    }
+
+    if (!this.lastItemForPagination.hasOwnProperty(this.hashKey)) {
+      return new Error(`lastItemForPagination does not contain '${this.hashKey}' field`)
+    }
+
+    if (this.rangeKey && !this.lastItemForPagination.hasOwnProperty(this.rangeKey)) {
+      return new Error(`lastItemForPagination does not contain '${this.rangeKey}' field`)
+    }
+
+    return undefined
+  }
+
+  private createLastItemKey(): Result<DocumentClient.Key | undefined> {
+    if (!this.lastItemForPagination) {
+      return undefined
+    }
+
+    const validationResult = this.validateLastItemForPagination()
+
+    if (isError(validationResult)) {
+      return validationResult
+    }
+
+    const lastItemKey = {
+      [this.partitionKey]: this.lastItemForPagination[this.partitionKey],
+      [this.hashKey]: this.lastItemForPagination[this.hashKey]
+    }
+
+    if (this.rangeKey) {
+      lastItemKey[this.rangeKey] = this.lastItemForPagination[this.rangeKey]
+    }
+
+    return lastItemKey
+  }
+
+  useIndex(indexName: string): IndexSearcher<TResult> {
+    this.indexName = indexName
+    return this
+  }
+
+  setIndexKeys(hashKey: string, hashKeyValue: unknown, rangeKey?: string): IndexSearcher<TResult> {
+    this.hashKey = hashKey
+    this.hashKeyValue = hashKeyValue
+    this.rangeKey = rangeKey
+    return this
+  }
+
+  paginate(
+    limit: number,
+    lastItemForPagination?: unknown | KeyValuePair<string, unknown>,
+    isAscendingOrder = false
+  ): IndexSearcher<TResult> {
+    this.limit = limit
+    this.lastItemForPagination = lastItemForPagination as KeyValuePair<string, unknown>
+    this.isAscendingOrder = isAscendingOrder
+    return this
+  }
+
+  async execute(): PromiseResult<TResult | undefined> {
+    const pagination: Pagination = {
+      limit: this.limit
+    }
+
+    const lastItemKey = this.createLastItemKey()
+
+    if (isError(lastItemKey)) {
+      return lastItemKey
+    }
+
+    pagination.lastItemKey = lastItemKey
+
+    const options: FetchByIndexOptions = {
+      indexName: this.indexName,
+      attributeName: this.hashKey,
+      attributeValue: this.hashKeyValue,
+      isAscendingOrder: this.isAscendingOrder,
+      pagination
+    }
+
+    const fetchResult = await this.gateway.fetchByIndex(this.tableName, options)
+
+    if (isError(fetchResult)) {
+      return fetchResult
+    }
+
+    return <TResult | undefined>fetchResult.Items
+  }
+}
