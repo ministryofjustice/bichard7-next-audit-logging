2afa523f84120be1b6d1541cf49496aade5e1b5d
Emad Karamad <emad.karamad@madetech.com>
2021-07-20 09:49:03 +0100

    Updated BichardAuditLogEvent fields to be readonly

--- shared/src/types/BichardAuditLogEvent.ts
+++ shared/src/types/BichardAuditLogEvent.ts
@@ -2,9 +2,9 @@ import AuditLogEvent from "./AuditLogEvent"
 import type BichardAuditLogEventOptions from "./BichardAuditLogEventOptions"
 
 export default class BichardAuditLogEvent extends AuditLogEvent {
-  public s3Path: string
+  public readonly s3Path: string
 
-  public eventSourceArn: string
+  public readonly eventSourceArn: string
 
   constructor(options: BichardAuditLogEventOptions) {
     super(options)
