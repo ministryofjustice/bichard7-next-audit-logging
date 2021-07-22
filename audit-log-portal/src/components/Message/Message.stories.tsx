10e6ce5f189c52ae8b15e7b88f058a05ae330409
MihaiMadeT <87318572+MihaiMadeT@users.noreply.github.com>
2021-07-22 11:04:21 +0100

    Adding stories and fixing tests

--- audit-log-portal/src/components/Message/Message.stories.tsx
+++ audit-log-portal/src/components/Message/Message.stories.tsx
@@ -1,6 +1,6 @@
 import type { Meta } from "@storybook/react"
 import { action } from "@storybook/addon-actions"
-import { AuditLog } from "shared"
+import { AuditLog, AuditLogStatus } from "shared"
 import Message from "./Message"
 
 export default {
@@ -13,5 +13,14 @@ export default {
   }
 } as Meta
 
-const message = new AuditLog("LIBRA-EXISS-0001", new Date(), "XML")
-export const Default = () => <Message message={message} onRetry={action("retry")} />
+const createMessage = (status: string) => {
+  const message = new AuditLog("LIBRA-EXISS-0001", new Date(), "XML")
+  message.status = status
+
+  return message
+}
+
+export const Processing = () => <Message message={createMessage(AuditLogStatus.processing)} onRetry={action("retry")} />
+export const Retrying = () => <Message message={createMessage(AuditLogStatus.retrying)} onRetry={action("retry")} />
+export const Completed = () => <Message message={createMessage(AuditLogStatus.completed)} onRetry={action("retry")} />
+export const Failed = () => <Message message={createMessage(AuditLogStatus.error)} onRetry={action("retry")} />
