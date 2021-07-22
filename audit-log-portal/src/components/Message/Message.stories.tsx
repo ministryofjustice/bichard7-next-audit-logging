bb37e9b50e42ed87f03d8aadfa9ab80671ca0a7e
MihaiMadeT <87318572+MihaiMadeT@users.noreply.github.com>
2021-07-22 10:52:30 +0100

    Update Message.stories

--- audit-log-portal/src/components/Message/Message.stories.tsx
+++ audit-log-portal/src/components/Message/Message.stories.tsx
@@ -1,11 +1,17 @@
 import type { Meta } from "@storybook/react"
+import { action } from "@storybook/addon-actions"
 import { AuditLog } from "shared"
 import Message from "./Message"
 
 export default {
   title: "components/Message",
-  component: Message
+  component: Message,
+  argTypes: {
+    onRetry: {
+      action: "retry"
+    }
+  }
 } as Meta
 
 const message = new AuditLog("LIBRA-EXISS-0001", new Date(), "XML")
-export const Default = () => <Message message={message} />
+export const Default = () => <Message message={message} onRetry={action("retry")} />
