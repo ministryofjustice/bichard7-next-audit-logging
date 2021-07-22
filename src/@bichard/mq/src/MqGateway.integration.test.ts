63e56907a518c1d66cec37d4f33390d4869c3bbf
Emad Karamad <emad.karamad@madetech.com>
2021-07-22 12:13:37 +0100

    Added shared/mq-gateway (#117)
    * Added shared/mq-gateway
    
    * Fixed deconstructServers test issue
    
    * Moved shared/mq-gateway to @bichard/mq
    
    * Moved reconnectOptions outside of MqGateway class
    
    * Removed defaultQueueName from MqConfig
    
    * Fixed mq tests
    
    * Updated sendMessage function to check the connection
    
    * Replaced optional chain operator with non-null assertion in MqGateway
    
    * Updated @bichard/mq path in projects file
    
    * Renamed @bichard/mq package name
    
    * Updated MqGateway.execute function to handle dispose error
    
    * Disabled no-non-null-assertion eslint rule
    
    * Refactored execute function in MqGateway class

---
+++ src/@bichard/mq/src/MqGateway.integration.test.ts
@@ -0,0 +1,36 @@
+import "@bichard/testing"
+import type MqConfig from "./MqConfig"
+import MqGateway from "./MqGateway"
+import TestMqGateway from "./TestMqGateway"
+
+jest.setTimeout(30000)
+
+const queueName = "mq-gateway-integration-testing"
+const config: MqConfig = {
+  url: "failover:(stomp://localhost:51613)",
+  username: "admin",
+  password: "admin"
+}
+
+const gateway = new MqGateway(config)
+const testGateway = new TestMqGateway(config)
+
+describe("MqGateway", () => {
+  afterAll(async () => {
+    await gateway.dispose()
+    await testGateway.dispose()
+  })
+
+  it("should create the queue and send the message", async () => {
+    const expectedMessage = '<?xml version="1.0" ?><root><element>value</element></root>'
+
+    const result = await gateway.execute(expectedMessage, queueName)
+
+    expect(result).toNotBeError()
+
+    const actualMessage = await testGateway.getMessage(queueName)
+    expect(actualMessage).toBeDefined()
+    expect(actualMessage).toNotBeError()
+    expect(actualMessage).toBe(expectedMessage)
+  })
+})
---
+++ src/@bichard/mq/src/index.ts
@@ -0,0 +1,3 @@
+export { default as MqGateway } from "./MqGateway"
+export { default as createMqConfig } from "./createMqConfig"
+export type { default as MqConfig } from "./MqConfig"
