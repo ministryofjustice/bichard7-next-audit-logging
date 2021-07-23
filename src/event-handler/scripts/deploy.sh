9fdf0f03d97219ae5cc454dc770106846e4583a3
Samuel Slade <samuel.slade@sladesoftware.co.uk>
2021-07-23 13:10:23 +0100

    Corrected template syntax for event handler state machine definition

--- src/event-handler/scripts/deploy.sh
+++ src/event-handler/scripts/deploy.sh
@@ -14,9 +14,9 @@ RECORD_EVENT_LAMBDA_ARN=$(get_lambda_arn "record-event")
 STATE_MACHINE_CONFIG_PATH=/tmp/event-handler-state-machine.json
 
 cat "$PWD/scripts/state-machine.json.tpl" | \
-  sed "s/{STORE_IN_S3_LAMBDA_ARN}/$STORE_IN_S3_LAMBDA_ARN/g" | \
-  sed "s/{TRANSLATE_EVENT_LAMBDA_ARN}/$TRANSLATE_EVENT_LAMBDA_ARN/g" | \
-  sed "s/{RECORD_EVENT_LAMBDA_ARN}/$RECORD_EVENT_LAMBDA_ARN/g" > "$STATE_MACHINE_CONFIG_PATH"
+  sed "s/\${STORE_IN_S3_LAMBDA_ARN}/$STORE_IN_S3_LAMBDA_ARN/g" | \
+  sed "s/\${TRANSLATE_EVENT_LAMBDA_ARN}/$TRANSLATE_EVENT_LAMBDA_ARN/g" | \
+  sed "s/\${RECORD_EVENT_LAMBDA_ARN}/$RECORD_EVENT_LAMBDA_ARN/g" > "$STATE_MACHINE_CONFIG_PATH"
 
 # Create the state machine (if it doesn't exist) using the config
 STATE_MACHINE_NAME=BichardEventHandler
