900e77e6c999629d59d26a3e73a603839aacee6e
Emad Karamad <emad.karamad@madetech.com>
2021-07-26 14:27:47 +0100

    Feature/bicaws 964 bichard s3 library (#120)
    * Added @bichard/s3 library
    
    * Added FakeS3Gateway
    
    * Updated S3 integration test for toNotBeError function
    
    * Updated AwsS3Gateway test
    
    * Tidied up @bichard/s3 module
    
    * Added parseGetObjectResponse
    
    * Removed redundant eslint-disable comment
    
    * Updated FakeS3Gateway to disable eslint rules for specific lines
    
    Co-authored-by: Samuel Slade <12849513+sladesamuel@users.noreply.github.com>

---
+++ src/@bichard/s3/src/AwsS3Gateway.integration.test.ts
@@ -0,0 +1,39 @@
+import "@bichard/testing"
+import AwsS3Gateway from "./AwsS3Gateway"
+import type S3Config from "./S3Config"
+import TestAwsS3Gateway from "./TestAwsS3Gateway"
+
+const config: S3Config = {
+  url: "http://localhost:4566",
+  region: "eu-west-2",
+  bucketName: "test-bucket"
+}
+const testGateway = new TestAwsS3Gateway(config)
+const gateway = new AwsS3Gateway(config)
+const fileName = "2021/04/09/12/06/123456.xml"
+
+describe("AwsS3Gateway", () => {
+  beforeAll(async () => {
+    await testGateway.createBucket()
+    await testGateway.deleteAll()
+  })
+
+  describe("getItem()", () => {
+    it("should return the contents of a file when it exists in the bucket", async () => {
+      await gateway.upload(fileName, "Message to be saved")
+
+      const content = await gateway.getItem(fileName)
+
+      expect(content).toBe("Message to be saved")
+    })
+  })
+
+  describe("upload()", () => {
+    it("should save the message in the bucket", async () => {
+      const result = await gateway.upload(fileName, "Message to be saved")
+
+      expect(result).toNotBeError()
+      expect(result).toBeUndefined()
+    })
+  })
+})
