eee075f14d3abd1ac74b668952da141d4b74d2c9
Emad Karamad <emad.karamad@madetech.com>
2021-07-26 17:29:37 +0100

    Broke down @bichard/testing to individual testing modules

--- src/@bichard/testing/dynamodb/src/index.ts
+++ src/@bichard/testing/dynamodb/src/index.ts
@@ -1,6 +1,2 @@
-import "./jest"
-
 // eslint-disable-next-line import/prefer-default-export
-export { default as invokeFunction } from "./invokeFunction"
 export { default as FakeAuditLogDynamoGateway } from "./FakeAuditLogDynamoGateway"
-export { default as FakeS3Gateway } from "./FakeS3Gateway"
