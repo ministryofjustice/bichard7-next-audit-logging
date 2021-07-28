dad699edc72c38562499ff58fd0f24730c173bbf
Emad Karamad <emad.karamad@madetech.com>
2021-07-28 18:54:58 +0100

    Refactored GetLastEventUseCase test

--- audit-log-api/src/use-cases/GetLastEventUseCase.test.ts
+++ audit-log-api/src/use-cases/GetLastEventUseCase.test.ts
@@ -1,22 +1,18 @@
 import "@bichard/testing-jest"
 import { FakeAuditLogDynamoGateway } from "@bichard/testing-dynamodb"
+import type { EventCategory } from "shared"
 import { AuditLog, AuditLogEvent, BichardAuditLogEvent, isError } from "shared"
 import GetLastEventUseCase from "./GetLastEventUseCase"
 
 const auditLogDynamoGateway = new FakeAuditLogDynamoGateway()
 const useCase = new GetLastEventUseCase(auditLogDynamoGateway)
 
-it("should return the last event when message exists and has events", async () => {
+const createAuditLog = (
+  eventCategory: EventCategory,
+  eventS3Path?: string,
+  eventSourceQueueName?: string
+): AuditLog => {
   const message = new AuditLog("External Correlation Id", new Date("2021-07-22T08:10:10"), "Xml")
-  const expectedEvent = new BichardAuditLogEvent({
-    category: "error",
-    timestamp: new Date("2021-07-22T12:10:10"),
-    eventType: "Expected Event type",
-    eventSource: "Expected Event Source",
-    s3Path: "Expected S3 Path",
-    eventSourceArn: "Expected Event Source ARN",
-    eventSourceQueueName: "Expected Queue Name"
-  })
 
   message.events = [
     new AuditLogEvent({
@@ -25,7 +21,15 @@ it("should return the last event when message exists and has events", async () =
       eventType: "Dummy Event Type",
       eventSource: "Dummy Event Source"
     }),
-    expectedEvent,
+    new BichardAuditLogEvent({
+      category: eventCategory,
+      timestamp: new Date("2021-07-22T12:10:10"),
+      eventType: "Expected Event type",
+      eventSource: "Expected Event Source",
+      s3Path: eventS3Path ?? "Expected S3 Path",
+      eventSourceArn: "Expected Event Source ARN",
+      eventSourceQueueName: eventSourceQueueName ?? "Expected Queue Name"
+    }),
     new AuditLogEvent({
       category: "information",
       timestamp: new Date("2021-07-22T10:10:10"),
@@ -34,30 +38,25 @@ it("should return the last event when message exists and has events", async () =
     })
   ]
 
+  return message
+}
+
+it("should return the last event when message exists and has events", async () => {
+  const message = createAuditLog("error")
+
   auditLogDynamoGateway.reset([message])
   const result = await useCase.get(message.messageId)
 
   expect(result).toNotBeError()
 
   const event = result as BichardAuditLogEvent
-  expect(event.s3Path).toBe(expectedEvent.s3Path)
-  expect(event.eventSourceArn).toBe(expectedEvent.eventSourceArn)
-  expect(event.eventSourceQueueName).toBe(expectedEvent.eventSourceQueueName)
+  expect(event.s3Path).toBe("Expected S3 Path")
+  expect(event.eventSourceArn).toBe("Expected Event Source ARN")
+  expect(event.eventSourceQueueName).toBe("Expected Queue Name")
 })
 
 it("should return error when last event category is not error", async () => {
-  const message = new AuditLog("External Correlation Id", new Date("2021-07-22T08:10:10"), "Xml")
-  message.events.push(
-    new BichardAuditLogEvent({
-      category: "information",
-      timestamp: new Date("2021-07-22T12:10:10"),
-      eventType: "Event type",
-      eventSource: "Event Source",
-      s3Path: "S3 Path",
-      eventSourceArn: "Event Source ARN",
-      eventSourceQueueName: "Queue Name"
-    })
-  )
+  const message = createAuditLog("information")
 
   auditLogDynamoGateway.reset([message])
   const result = await useCase.get(message.messageId)
@@ -69,15 +68,7 @@ it("should return error when last event category is not error", async () => {
 })
 
 it("should return error when last event does not have S3 path and queue name", async () => {
-  const message = new AuditLog("External Correlation Id", new Date("2021-07-22T08:10:10"), "Xml")
-  message.events.push(
-    new AuditLogEvent({
-      category: "error",
-      timestamp: new Date("2021-07-22T12:10:10"),
-      eventType: "Event type",
-      eventSource: "Event Source"
-    })
-  )
+  const message = createAuditLog("error", "", "")
 
   auditLogDynamoGateway.reset([message])
   const result = await useCase.get(message.messageId)
@@ -89,7 +80,8 @@ it("should return error when last event does not have S3 path and queue name", a
 })
 
 it("should return error when there are no events against the message", async () => {
-  const message = new AuditLog("External Correlation Id", new Date("2021-07-22T08:10:10"), "Xml")
+  const message = createAuditLog("error")
+  message.events = []
 
   auditLogDynamoGateway.reset([message])
   const result = await useCase.get(message.messageId)
