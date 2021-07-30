7b8eafc7e41cfc4e6a5219dd3c2c55f0e8d0edbd
Emad Karamad <emad.karamad@madetech.com>
2021-07-30 15:18:03 +0100

    Updated getMessageStatus to return error when category is error

--- shared/src/AuditLogDynamoGateway/getMessageStatus.ts
+++ shared/src/AuditLogDynamoGateway/getMessageStatus.ts
@@ -2,11 +2,13 @@ import type { AuditLogEvent } from "../types"
 import { AuditLogStatus } from "../types"
 
 const getMessageStatus = (event: AuditLogEvent): string => {
+  if (event.category === "error") {
+    return AuditLogStatus.error
+  }
+
   switch (event.eventType) {
     case "PNC Response received":
       return AuditLogStatus.completed
-    case "PNC Response not received":
-      return AuditLogStatus.error
     case "Retrying failed message":
       return AuditLogStatus.retrying
     default:
