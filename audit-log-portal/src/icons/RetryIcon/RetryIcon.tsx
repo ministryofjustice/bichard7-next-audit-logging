b34a4bbea5e268f3f4106e380ac3a87675537f05
Emad Karamad <emad.karamad@madetech.com>
2021-07-30 13:55:07 +0100

    Refactored RetryIcon animation

--- audit-log-portal/src/icons/RetryIcon/RetryIcon.tsx
+++ audit-log-portal/src/icons/RetryIcon/RetryIcon.tsx
@@ -1,19 +1,24 @@
 import { memo } from "react"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faRedo } from "@fortawesome/free-solid-svg-icons"
+import styled from "styled-components"
 
 interface Props {
   spin?: boolean
 }
 
+const RetryFontAwesomeIcon = styled(FontAwesomeIcon)`
+  animation: ${(props) => (props.spin ? "rotate-clockwise linear 0.5s infinite" : "none")};
+`
+
 const RetryIcon = ({ spin }: Props) => (
-  <FontAwesomeIcon
+  <RetryFontAwesomeIcon
     icon={faRedo}
     style={{
       width: 18,
       height: 18
     }}
-    className={spin ? "retry-icon__spinning" : ""}
+    spin={spin}
   />
 )
 
