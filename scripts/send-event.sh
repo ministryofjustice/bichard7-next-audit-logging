bb4e767dd6f39c74b7248d9f332a3ca025f58b6b
Emad Karamad <emad.karamad@madetech.com>
2021-07-30 15:24:40 +0100

    Updated send event script to support different event types

---
+++ scripts/send-event.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+set -e
+
+EVENT_NAME=$1
+QUEUE_NAME=$2
+EVENT_DATE_TIME=$(date -u +"%Y-%m-%dT%H:%M:%SZ")
+
+if [[ -z $EVENT_NAME ]] || [[ -z $QUEUE_NAME ]]
+then
+    echo "ERROR: Script is expecting 2 parameters"
+    exit 1
+fi
+
+if [[ -z $MESSAGE_ID ]]
+then
+    echo "ERROR: Script requires MESSAGE_ID environment variable"
+    exit 1
+fi
+
+MESSAGE_DATA=$(cat "events/$EVENT_NAME.xml" | \
+  sed "s#{MESSAGE_ID}#$MESSAGE_ID#g" | \
+  sed "s#{EVENT_TYPE}#$EVENT_TYPE#g" | \
+  sed "s#{EVENT_DATE_TIME}#$EVENT_DATE_TIME#g" | \
+  base64)
+
+PAYLOAD=$(cat <<- EOM
+{
+  "eventSource": "$EVENT_NAME",
+  "eventSourceArn": "$EVENT_NAME",
+  "messages": [
+    {
+      "messageID": "",
+      "messageType": "FailureEvent",
+      "data": "$MESSAGE_DATA",
+      "destination": {
+        "physicalName": "$QUEUE_NAME"
+      }
+    }
+  ]
+}
+EOM
+)
+
+curl -d "body=$MESSAGE_DATA" "http://admin:admin@localhost:58161/api/message/$QUEUE_NAME?type=queue"
+
+awslocal lambda invoke \
+  --function-name "$EVENT_NAME-receiver" \
+  --invocation-type Event \
+  --payload "$PAYLOAD" \
+  output.json
