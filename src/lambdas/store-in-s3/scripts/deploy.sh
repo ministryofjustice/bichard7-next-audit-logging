b2387feb2970bf5c0e03ed33eafb3015f433813a
Emad Karamad <emad.karamad@madetech.com>
2021-08-02 11:07:03 +0100

    Feature/bicaws 964 fixes and improvements (#135)
    * Updated send event script to support different event types
    
    * Fixed issue with getting incoming message handler state machine arn
    
    * Updated store-in-s3 default bucket name
    
    * Updated incoming message handler bucket name
    
    * Removed redundant command
    
    * Fixed issue with stote-in-s3 e2e test
    
    * Added PNC Response received event xml

--- src/lambdas/store-in-s3/scripts/deploy.sh
+++ src/lambdas/store-in-s3/scripts/deploy.sh
@@ -5,7 +5,7 @@ set -e
 source "$PWD/../../../environment/create-lambda.sh"
 
 # Make sure the S3 bucket exists
-BUCKET_NAME=store-in-s3
+BUCKET_NAME=audit-log-events
 TMP_ENV_VARS=/tmp/store-in-s3-env-vars.json
 
 cat "$PWD/scripts/env-vars.json.tpl" | sed "s/{EVENTS_BUCKET_NAME}/$BUCKET_NAME/g" > "$TMP_ENV_VARS"
