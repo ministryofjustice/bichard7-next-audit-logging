a6063b4a08bda44de0afb2bd5b904507387b55f8
Emad Karamad <emad.karamad@madetech.com>
2021-08-03 09:39:31 +0100

    Updated retry message to decode the message before pushing to mq

--- audit-log-api/src/use-cases/SendMessageToQueueUseCase.ts
+++ audit-log-api/src/use-cases/SendMessageToQueueUseCase.ts
@@ -1,10 +1,12 @@
 import type { MqGateway } from "@bichard/mq"
 import type { PromiseResult } from "shared"
+import { decodeBase64 } from "shared"
 
 export default class SendMessageToQueueUseCase {
   constructor(private mqGateway: MqGateway) {}
 
   send(queueName: string, message: string): PromiseResult<void> {
-    return this.mqGateway.execute(message, queueName)
+    const decodedMessage = decodeBase64(message)
+    return this.mqGateway.execute(decodedMessage, queueName)
   }
 }
