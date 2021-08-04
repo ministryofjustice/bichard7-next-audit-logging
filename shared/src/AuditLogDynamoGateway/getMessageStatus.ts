918371d2792c5637827bd38522cb40a248516db7
Emad Karamad <emad.karamad@madetech.com>
2021-08-04 12:39:15 +0100

    Updated getMessageStatus to return null if status should not change (#137)
    * Updated getMessageStatus to return null if status should not change
    
    * Fixed indentation in AwsAuditLogDynamoGateway

--- shared/src/AuditLogDynamoGateway/getMessageStatus.ts
+++ shared/src/AuditLogDynamoGateway/getMessageStatus.ts
@@ -1,18 +1,21 @@
 import type { AuditLogEvent } from "../types"
 import { AuditLogStatus } from "../types"
 
-const getMessageStatus = (event: AuditLogEvent): string => {
+const getMessageStatus = (event: AuditLogEvent): string | null => {
   if (event.category === "error") {
     return AuditLogStatus.error
   }
 
   switch (event.eventType) {
     case "PNC Response received":
+    case "PNC Update applied successfully":
       return AuditLogStatus.completed
     case "Retrying failed message":
       return AuditLogStatus.retrying
-    default:
+    case "Message Sent to Bichard":
       return AuditLogStatus.processing
+    default:
+      return null
   }
 }
 
