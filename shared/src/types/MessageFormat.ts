b482d877d67f0e814edd9c27f00bc52727dfd1f7
Emad Karamad <emad.karamad@madetech.com>
2021-10-21 10:01:17 +0100

    Added pnc update request message format

--- shared/src/types/MessageFormat.ts
+++ shared/src/types/MessageFormat.ts
@@ -1,3 +1,9 @@
-type MessageFormat = "AuditEvent" | "GeneralEvent" | "CourtResultInput" | "DataSetPncUpdate" | "HearingOutcomePncUpdate"
+type MessageFormat =
+  | "AuditEvent"
+  | "GeneralEvent"
+  | "CourtResultInput"
+  | "DataSetPncUpdate"
+  | "HearingOutcomePncUpdate"
+  | "PncUpdateRequest"
 
 export default MessageFormat
