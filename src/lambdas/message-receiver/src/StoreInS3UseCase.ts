eaa01b1b9fd1668b52e811c880841e5c39e20bbb
Emad Karamad <emad.karamad@madetech.com>
2021-12-02 11:49:12 +0000

    Fixed month in the generated S3 key in message-receiver

--- src/lambdas/message-receiver/src/StoreInS3UseCase.ts
+++ src/lambdas/message-receiver/src/StoreInS3UseCase.ts
@@ -26,7 +26,7 @@ export default class StoreInS3UseCase {
   private static getMessageFilename(messageFormat: MessageFormat): string {
     const now = new Date()
     const year = now.getFullYear()
-    const month = now.getMonth()
+    const month = now.getMonth() + 1
     const day = now.getDay()
     const hour = now.getHours()
     const minute = now.getMinutes()
