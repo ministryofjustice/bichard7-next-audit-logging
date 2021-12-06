73754b5fc03ca944088d8b225f96e65dfd906205
Emad Karamad <emad.karamad@madetech.com>
2021-12-06 12:31:24 +0000

    Add source hash to S3 object metadata

--- scripts/codebuild-artifacts.sh
+++ scripts/codebuild-artifacts.sh
@@ -11,10 +11,12 @@ function upload_to_s3 {
     contentType="application/octet-stream"
   fi
 
+  sourceHash=$(openssl dgst -binary -sha256 "$sourceFilename" | openssl base64)
   aws s3 cp "$sourceFilename" \
     "s3://$ARTIFACT_BUCKET/audit-logging/$destinationFilename" \
     --content-type "$contentType" \
     --acl bucket-owner-full-control
+    --metadata hash="$sourceHash"
 }
 
 ############################################
@@ -52,16 +54,15 @@ zip retrieveFromS3.zip retrieveFromS3.js
 zip sendToBichard.zip sendToBichard.js
 zip recordSentToBichardEvent.zip recordSentToBichardEvent.js
 
-cd -
-
 # Upload all artifacts to the S3 bucket
-aws s3 cp \
-  ./incoming-message-handler/build/ \
-  s3://$ARTIFACT_BUCKET/audit-logging/ \
-  --recursive \
-  --exclude "*" \
-  --include "*.zip" \
-  --acl bucket-owner-full-control
+upload_to_s3 formatMessage.zip formatMessage.zip
+upload_to_s3 logMessageReceipt.zip logMessageReceipt.js
+upload_to_s3 parseMessage.zip parseMessage.js
+upload_to_s3 retrieveFromS3.zip retrieveFromS3.js
+upload_to_s3 sendToBichard.zip sendToBichard.js
+upload_to_s3 recordSentToBichardEvent.zip recordSentToBichardEvent.js
+
+cd -
 
 upload_to_s3 "./incoming-message-handler/scripts/state-machine.json.tpl" "incoming-message-handler-state-machine.json.tpl" "application/json"
 
@@ -78,16 +79,13 @@ zip createAuditLogEvent.zip createAuditLogEvent.js
 zip getEvents.zip getEvents.js
 zip retryMessage.zip retryMessage.js
 
-cd -
+upload_to_s3 getMessages.zip getMessages.js
+upload_to_s3 createAuditLog.zip createAuditLog.js
+upload_to_s3 createAuditLogEvent.zip createAuditLogEvent.js
+upload_to_s3 getEvents.zip getEvents.js
+upload_to_s3 retryMessage.zip retryMessage.js
 
-# Upload all artifacts to the S3 bucket
-aws s3 cp \
-  ./audit-log-api/build/ \
-  s3://$ARTIFACT_BUCKET/audit-logging/ \
-  --recursive \
-  --exclude "*" \
-  --include "*.zip" \
-  --acl bucket-owner-full-control
+cd -
 
 ############################################
 # Event Handler
@@ -102,4 +100,3 @@ upload_to_s3 "src/event-handler/scripts/state-machine.json.tpl" "event-handler-s
 
 # Build the Portal Docker Image
 make codebuild-portal-image
-
