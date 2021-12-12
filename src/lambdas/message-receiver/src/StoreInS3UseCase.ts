e526d62ebfaaf8fa4c8284f3a18863b7261686d7
Ben Pirt <ben@pirt.co.uk>
2021-12-12 15:03:21 +0000

    Fix the timestamp used to store received messages

--- src/lambdas/message-receiver/src/StoreInS3UseCase.ts
+++ src/lambdas/message-receiver/src/StoreInS3UseCase.ts
@@ -26,10 +26,10 @@ export default class StoreInS3UseCase {
   private static getMessageFilename(messageFormat: MessageFormat): string {
     const now = new Date()
     const year = now.getFullYear()
-    const month = now.getMonth() + 1
-    const day = now.getDay()
-    const hour = now.getHours()
-    const minute = now.getMinutes()
+    const month = (now.getMonth() + 1).toString().padStart(2, "0")
+    const day = now.getDate().toString().padStart(2, "0")
+    const hour = now.getHours().toString().padStart(2, "0")
+    const minute = now.getMinutes().toString().padStart(2, "0")
 
     return `${year}/${month}/${day}/${hour}/${minute}/${messageFormat}-${uuid()}.json`
   }
