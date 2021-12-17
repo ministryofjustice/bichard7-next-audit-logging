2d8be3e5f072f008d7745662370644238bbb06d5
Ben Pirt <ben@pirt.co.uk>
2021-12-17 16:03:54 +0000

    Make the fractional part of the timestamp optional and not requiring exactly 3 digits

--- audit-log-api/src/utils/isIsoDate.ts
+++ audit-log-api/src/utils/isIsoDate.ts
@@ -1,5 +1,5 @@
 export default (dateString: string): boolean => {
-  if (!/\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}.\d{3}Z/.test(dateString)) {
+  if (!/\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}(\.\d+)?Z/.test(dateString)) {
     return false
   }
 
