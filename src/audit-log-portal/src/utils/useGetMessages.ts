a68e6e65ca5843569ed410ce823420c465b5dc32
Emad Karamad <emad.karamad@madetech.com>
2022-02-02 11:25:58 +0000

    Fix formatting issue

--- src/audit-log-portal/src/utils/useGetMessages.ts
+++ src/audit-log-portal/src/utils/useGetMessages.ts
@@ -1,4 +1,4 @@
-import useSWRInfinite from 'swr/infinite'
+import useSWRInfinite from "swr/infinite"
 import type { KeyLoader } from "swr/dist/types"
 import type { AuditLog } from "shared-types"
 import fetcher from "./fetcher"
