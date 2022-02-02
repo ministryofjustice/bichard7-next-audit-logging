69c951aa531d48b4d7a523e7ae34da530055059a
Emad Karamad <emad.karamad@madetech.com>
2022-02-02 11:15:38 +0000

    Update mocked API handlers and data

--- src/audit-log-portal/mocks/data/messages.js
+++ src/audit-log-portal/mocks/data/messages.js
@@ -151,6 +151,581 @@ module.exports = {
           attributes: {}
         }
       ]
+    },
+    {
+      messageId: "3079c4ff-d24a-45fc-92ec-852316940e40-8",
+      externalCorrelationId: "Message8",
+      receivedDate: "2021-11-13T17:12:00.000Z",
+      messageXml: "XML-5",
+      caseId: "6000",
+      status: "Completed",
+      lastEventType: "PNC Response received",
+      events: [
+        {
+          category: "information",
+          eventType: "Message Sent to Bichard",
+          eventSource: "Audit logging",
+          timestamp: "2021-11-13T17:14:00.000Z",
+          attributes: {}
+        },
+        {
+          category: "information",
+          eventType: "PNC Response received",
+          eventSource: "Bichard",
+          timestamp: "2021-11-13T17:16:00.000Z",
+          attributes: {}
+        }
+      ]
+    },
+    {
+      messageId: "3079c4ff-d24a-45fc-92ec-852316940e40-9",
+      externalCorrelationId: "Message9",
+      receivedDate: "2021-11-13T17:12:00.000Z",
+      messageXml: "XML-5",
+      caseId: "6000",
+      status: "Completed",
+      lastEventType: "PNC Response received",
+      events: [
+        {
+          category: "information",
+          eventType: "Message Sent to Bichard",
+          eventSource: "Audit logging",
+          timestamp: "2021-11-13T17:14:00.000Z",
+          attributes: {}
+        },
+        {
+          category: "information",
+          eventType: "PNC Response received",
+          eventSource: "Bichard",
+          timestamp: "2021-11-13T17:16:00.000Z",
+          attributes: {}
+        }
+      ]
+    },
+    {
+      messageId: "3079c4ff-d24a-45fc-92ec-852316940e43-10",
+      externalCorrelationId: "Message10",
+      receivedDate: "2021-11-13T17:12:00.000Z",
+      messageXml: "XML-5",
+      caseId: "6000",
+      status: "Completed",
+      lastEventType: "PNC Response received",
+      events: [
+        {
+          category: "information",
+          eventType: "Message Sent to Bichard",
+          eventSource: "Audit logging",
+          timestamp: "2021-11-13T17:14:00.000Z",
+          attributes: {}
+        },
+        {
+          category: "information",
+          eventType: "PNC Response received",
+          eventSource: "Bichard",
+          timestamp: "2021-11-13T17:16:00.000Z",
+          attributes: {}
+        }
+      ]
+    },
+    {
+      messageId: "3079c4ff-d24a-45fc-92ec-852316940e40-11",
+      externalCorrelationId: "Message11",
+      receivedDate: "2021-11-13T17:12:00.000Z",
+      messageXml: "XML-5",
+      caseId: "6000",
+      status: "Completed",
+      lastEventType: "PNC Response received",
+      events: [
+        {
+          category: "information",
+          eventType: "Message Sent to Bichard",
+          eventSource: "Audit logging",
+          timestamp: "2021-11-13T17:14:00.000Z",
+          attributes: {}
+        },
+        {
+          category: "information",
+          eventType: "PNC Response received",
+          eventSource: "Bichard",
+          timestamp: "2021-11-13T17:16:00.000Z",
+          attributes: {}
+        }
+      ]
+    },
+    {
+      messageId: "3079c4ff-d24a-45fc-92ec-852316940e40-12",
+      externalCorrelationId: "Message12",
+      receivedDate: "2021-11-13T17:12:00.000Z",
+      messageXml: "XML-5",
+      caseId: "6000",
+      status: "Completed",
+      lastEventType: "PNC Response received",
+      events: [
+        {
+          category: "information",
+          eventType: "Message Sent to Bichard",
+          eventSource: "Audit logging",
+          timestamp: "2021-11-13T17:14:00.000Z",
+          attributes: {}
+        },
+        {
+          category: "information",
+          eventType: "PNC Response received",
+          eventSource: "Bichard",
+          timestamp: "2021-11-13T17:16:00.000Z",
+          attributes: {}
+        }
+      ]
+    },
+    {
+      messageId: "3079c4ff-d24a-45fc-92ec-852316940e40-13",
+      externalCorrelationId: "Message13",
+      receivedDate: "2021-11-13T17:12:00.000Z",
+      messageXml: "XML-5",
+      caseId: "6000",
+      status: "Completed",
+      lastEventType: "PNC Response received",
+      events: [
+        {
+          category: "information",
+          eventType: "Message Sent to Bichard",
+          eventSource: "Audit logging",
+          timestamp: "2021-11-13T17:14:00.000Z",
+          attributes: {}
+        },
+        {
+          category: "information",
+          eventType: "PNC Response received",
+          eventSource: "Bichard",
+          timestamp: "2021-11-13T17:16:00.000Z",
+          attributes: {}
+        }
+      ]
+    },
+    {
+      messageId: "3079c4ff-d24a-45fc-92ec-852316940e40-14",
+      externalCorrelationId: "Message14",
+      receivedDate: "2021-11-13T17:12:00.000Z",
+      messageXml: "XML-5",
+      caseId: "6000",
+      status: "Completed",
+      lastEventType: "PNC Response received",
+      events: [
+        {
+          category: "information",
+          eventType: "Message Sent to Bichard",
+          eventSource: "Audit logging",
+          timestamp: "2021-11-13T17:14:00.000Z",
+          attributes: {}
+        },
+        {
+          category: "information",
+          eventType: "PNC Response received",
+          eventSource: "Bichard",
+          timestamp: "2021-11-13T17:16:00.000Z",
+          attributes: {}
+        }
+      ]
+    },
+    {
+      messageId: "3079c4ff-d24a-45fc-92ec-852316940e40-15",
+      externalCorrelationId: "Message15",
+      receivedDate: "2021-11-13T17:12:00.000Z",
+      messageXml: "XML-5",
+      caseId: "6000",
+      status: "Completed",
+      lastEventType: "PNC Response received",
+      events: [
+        {
+          category: "information",
+          eventType: "Message Sent to Bichard",
+          eventSource: "Audit logging",
+          timestamp: "2021-11-13T17:14:00.000Z",
+          attributes: {}
+        },
+        {
+          category: "information",
+          eventType: "PNC Response received",
+          eventSource: "Bichard",
+          timestamp: "2021-11-13T17:16:00.000Z",
+          attributes: {}
+        }
+      ]
+    },
+    {
+      messageId: "3079c4ff-d24a-45fc-92ec-852316940e40-16",
+      externalCorrelationId: "Message16",
+      receivedDate: "2021-11-13T17:12:00.000Z",
+      messageXml: "XML-5",
+      caseId: "6000",
+      status: "Completed",
+      lastEventType: "PNC Response received",
+      events: [
+        {
+          category: "information",
+          eventType: "Message Sent to Bichard",
+          eventSource: "Audit logging",
+          timestamp: "2021-11-13T17:14:00.000Z",
+          attributes: {}
+        },
+        {
+          category: "information",
+          eventType: "PNC Response received",
+          eventSource: "Bichard",
+          timestamp: "2021-11-13T17:16:00.000Z",
+          attributes: {}
+        }
+      ]
+    },
+    {
+      messageId: "3079c4ff-d24a-45fc-92ec-852316940e40-17",
+      externalCorrelationId: "Message17",
+      receivedDate: "2021-11-13T17:12:00.000Z",
+      messageXml: "XML-5",
+      caseId: "6000",
+      status: "Completed",
+      lastEventType: "PNC Response received",
+      events: [
+        {
+          category: "information",
+          eventType: "Message Sent to Bichard",
+          eventSource: "Audit logging",
+          timestamp: "2021-11-13T17:14:00.000Z",
+          attributes: {}
+        },
+        {
+          category: "information",
+          eventType: "PNC Response received",
+          eventSource: "Bichard",
+          timestamp: "2021-11-13T17:16:00.000Z",
+          attributes: {}
+        }
+      ]
+    },
+    {
+      messageId: "3079c4ff-d24a-45fc-92ec-852316940e40-18",
+      externalCorrelationId: "Message18",
+      receivedDate: "2021-11-13T17:12:00.000Z",
+      messageXml: "XML-5",
+      caseId: "6000",
+      status: "Completed",
+      lastEventType: "PNC Response received",
+      events: [
+        {
+          category: "information",
+          eventType: "Message Sent to Bichard",
+          eventSource: "Audit logging",
+          timestamp: "2021-11-13T17:14:00.000Z",
+          attributes: {}
+        },
+        {
+          category: "information",
+          eventType: "PNC Response received",
+          eventSource: "Bichard",
+          timestamp: "2021-11-13T17:16:00.000Z",
+          attributes: {}
+        }
+      ]
+    },
+    {
+      messageId: "3079c4ff-d24a-45fc-92ec-852316940e40-19",
+      externalCorrelationId: "Message19",
+      receivedDate: "2021-11-13T17:12:00.000Z",
+      messageXml: "XML-5",
+      caseId: "6000",
+      status: "Completed",
+      lastEventType: "PNC Response received",
+      events: [
+        {
+          category: "information",
+          eventType: "Message Sent to Bichard",
+          eventSource: "Audit logging",
+          timestamp: "2021-11-13T17:14:00.000Z",
+          attributes: {}
+        },
+        {
+          category: "information",
+          eventType: "PNC Response received",
+          eventSource: "Bichard",
+          timestamp: "2021-11-13T17:16:00.000Z",
+          attributes: {}
+        }
+      ]
+    },
+    {
+      messageId: "3079c4ff-d24a-45fc-92ec-852316940e40-20",
+      externalCorrelationId: "Message20",
+      receivedDate: "2021-11-13T17:12:00.000Z",
+      messageXml: "XML-5",
+      caseId: "6000",
+      status: "Completed",
+      lastEventType: "PNC Response received",
+      events: [
+        {
+          category: "information",
+          eventType: "Message Sent to Bichard",
+          eventSource: "Audit logging",
+          timestamp: "2021-11-13T17:14:00.000Z",
+          attributes: {}
+        },
+        {
+          category: "information",
+          eventType: "PNC Response received",
+          eventSource: "Bichard",
+          timestamp: "2021-11-13T17:16:00.000Z",
+          attributes: {}
+        }
+      ]
+    },
+    {
+      messageId: "3079c4ff-d24a-45fc-92ec-852316940e40-21",
+      externalCorrelationId: "Message21",
+      receivedDate: "2021-11-13T17:12:00.000Z",
+      messageXml: "XML-5",
+      caseId: "6000",
+      status: "Completed",
+      lastEventType: "PNC Response received",
+      events: [
+        {
+          category: "information",
+          eventType: "Message Sent to Bichard",
+          eventSource: "Audit logging",
+          timestamp: "2021-11-13T17:14:00.000Z",
+          attributes: {}
+        },
+        {
+          category: "information",
+          eventType: "PNC Response received",
+          eventSource: "Bichard",
+          timestamp: "2021-11-13T17:16:00.000Z",
+          attributes: {}
+        }
+      ]
+    },
+    {
+      messageId: "3079c4ff-d24a-45fc-92ec-852316940e40-22",
+      externalCorrelationId: "Message22",
+      receivedDate: "2021-11-13T17:12:00.000Z",
+      messageXml: "XML-5",
+      caseId: "6000",
+      status: "Completed",
+      lastEventType: "PNC Response received",
+      events: [
+        {
+          category: "information",
+          eventType: "Message Sent to Bichard",
+          eventSource: "Audit logging",
+          timestamp: "2021-11-13T17:14:00.000Z",
+          attributes: {}
+        },
+        {
+          category: "information",
+          eventType: "PNC Response received",
+          eventSource: "Bichard",
+          timestamp: "2021-11-13T17:16:00.000Z",
+          attributes: {}
+        }
+      ]
+    },
+    {
+      messageId: "3079c4ff-d24a-45fc-92ec-852316940e40-23",
+      externalCorrelationId: "Message23",
+      receivedDate: "2021-11-13T17:12:00.000Z",
+      messageXml: "XML-5",
+      caseId: "6000",
+      status: "Completed",
+      lastEventType: "PNC Response received",
+      events: [
+        {
+          category: "information",
+          eventType: "Message Sent to Bichard",
+          eventSource: "Audit logging",
+          timestamp: "2021-11-13T17:14:00.000Z",
+          attributes: {}
+        },
+        {
+          category: "information",
+          eventType: "PNC Response received",
+          eventSource: "Bichard",
+          timestamp: "2021-11-13T17:16:00.000Z",
+          attributes: {}
+        }
+      ]
+    },
+    {
+      messageId: "3079c4ff-d24a-45fc-92ec-852316940e40-24",
+      externalCorrelationId: "Message24",
+      receivedDate: "2021-11-13T17:12:00.000Z",
+      messageXml: "XML-5",
+      caseId: "6000",
+      status: "Completed",
+      lastEventType: "PNC Response received",
+      events: [
+        {
+          category: "information",
+          eventType: "Message Sent to Bichard",
+          eventSource: "Audit logging",
+          timestamp: "2021-11-13T17:14:00.000Z",
+          attributes: {}
+        },
+        {
+          category: "information",
+          eventType: "PNC Response received",
+          eventSource: "Bichard",
+          timestamp: "2021-11-13T17:16:00.000Z",
+          attributes: {}
+        }
+      ]
+    },
+    {
+      messageId: "3079c4ff-d24a-45fc-92ec-852316940e40-25",
+      externalCorrelationId: "Message25",
+      receivedDate: "2021-11-13T17:12:00.000Z",
+      messageXml: "XML-5",
+      caseId: "6000",
+      status: "Completed",
+      lastEventType: "PNC Response received",
+      events: [
+        {
+          category: "information",
+          eventType: "Message Sent to Bichard",
+          eventSource: "Audit logging",
+          timestamp: "2021-11-13T17:14:00.000Z",
+          attributes: {}
+        },
+        {
+          category: "information",
+          eventType: "PNC Response received",
+          eventSource: "Bichard",
+          timestamp: "2021-11-13T17:16:00.000Z",
+          attributes: {}
+        }
+      ]
+    },
+    {
+      messageId: "3079c4ff-d24a-45fc-92ec-852316940e40-26",
+      externalCorrelationId: "Message26",
+      receivedDate: "2021-11-13T17:12:00.000Z",
+      messageXml: "XML-5",
+      caseId: "6000",
+      status: "Completed",
+      lastEventType: "PNC Response received",
+      events: [
+        {
+          category: "information",
+          eventType: "Message Sent to Bichard",
+          eventSource: "Audit logging",
+          timestamp: "2021-11-13T17:14:00.000Z",
+          attributes: {}
+        },
+        {
+          category: "information",
+          eventType: "PNC Response received",
+          eventSource: "Bichard",
+          timestamp: "2021-11-13T17:16:00.000Z",
+          attributes: {}
+        }
+      ]
+    },
+    {
+      messageId: "3079c4ff-d24a-45fc-92ec-852316940e40-27",
+      externalCorrelationId: "Message27",
+      receivedDate: "2021-11-13T17:12:00.000Z",
+      messageXml: "XML-5",
+      caseId: "6000",
+      status: "Completed",
+      lastEventType: "PNC Response received",
+      events: [
+        {
+          category: "information",
+          eventType: "Message Sent to Bichard",
+          eventSource: "Audit logging",
+          timestamp: "2021-11-13T17:14:00.000Z",
+          attributes: {}
+        },
+        {
+          category: "information",
+          eventType: "PNC Response received",
+          eventSource: "Bichard",
+          timestamp: "2021-11-13T17:16:00.000Z",
+          attributes: {}
+        }
+      ]
+    },
+    {
+      messageId: "3079c4ff-d24a-45fc-92ec-852316940e40-28",
+      externalCorrelationId: "Message28",
+      receivedDate: "2021-11-13T17:12:00.000Z",
+      messageXml: "XML-5",
+      caseId: "6000",
+      status: "Completed",
+      lastEventType: "PNC Response received",
+      events: [
+        {
+          category: "information",
+          eventType: "Message Sent to Bichard",
+          eventSource: "Audit logging",
+          timestamp: "2021-11-13T17:14:00.000Z",
+          attributes: {}
+        },
+        {
+          category: "information",
+          eventType: "PNC Response received",
+          eventSource: "Bichard",
+          timestamp: "2021-11-13T17:16:00.000Z",
+          attributes: {}
+        }
+      ]
+    },
+    {
+      messageId: "3079c4ff-d24a-45fc-92ec-852316940e40-29",
+      externalCorrelationId: "Message29",
+      receivedDate: "2021-11-13T17:12:00.000Z",
+      messageXml: "XML-5",
+      caseId: "6000",
+      status: "Completed",
+      lastEventType: "PNC Response received",
+      events: [
+        {
+          category: "information",
+          eventType: "Message Sent to Bichard",
+          eventSource: "Audit logging",
+          timestamp: "2021-11-13T17:14:00.000Z",
+          attributes: {}
+        },
+        {
+          category: "information",
+          eventType: "PNC Response received",
+          eventSource: "Bichard",
+          timestamp: "2021-11-13T17:16:00.000Z",
+          attributes: {}
+        }
+      ]
+    },
+    {
+      messageId: "3079c4ff-d24a-45fc-92ec-852316940e40-30",
+      externalCorrelationId: "Message30",
+      receivedDate: "2021-11-13T17:12:00.000Z",
+      messageXml: "XML-5",
+      caseId: "6000",
+      status: "Completed",
+      lastEventType: "PNC Response received",
+      events: [
+        {
+          category: "information",
+          eventType: "Message Sent to Bichard",
+          eventSource: "Audit logging",
+          timestamp: "2021-11-13T17:14:00.000Z",
+          attributes: {}
+        },
+        {
+          category: "information",
+          eventType: "PNC Response received",
+          eventSource: "Bichard",
+          timestamp: "2021-11-13T17:16:00.000Z",
+          attributes: {}
+        }
+      ]
     }
   ]
 }
