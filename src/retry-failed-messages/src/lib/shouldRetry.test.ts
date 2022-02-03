30d30e034620af4e15c28a6d1c8d29cdd46d6993
Ben Pirt <ben@pirt.co.uk>
2022-02-03 17:25:18 +0000

    Update the tests

--- src/retry-failed-messages/src/lib/shouldRetry.test.ts
+++ src/retry-failed-messages/src/lib/shouldRetry.test.ts
@@ -5,20 +5,20 @@ import { mockAuditLog, mockAuditLogEvent } from "shared-testing"
 
 describe("shouldRetry", () => {
   it("should not retry new messages too early", () => {
-    const receivedDate = new Date(Date.now() - 1799)
+    const receivedDate = new Date(Date.now() - 1799000)
     const message = mockAuditLog(receivedDate)
     expect(shouldRetry(message)).toBeFalsy()
   })
 
   it("should correctly retry new messages after an initial wait", () => {
-    const receivedDate = new Date(Date.now() - 1801)
+    const receivedDate = new Date(Date.now() - 1801000)
     const message = mockAuditLog(receivedDate)
     expect(shouldRetry(message)).toBeTruthy()
   })
 
   it("should correctly handle messages that have already been retried", () => {
     const message = mockAuditLog()
-    const event = mockAuditLogEvent("information", "Retrying failed message", new Date(Date.now() - 86401))
+    const event = mockAuditLogEvent("information", "Retrying failed message", new Date(Date.now() - 86401000))
     message.events.push(event)
     expect(shouldRetry(message)).toBeTruthy()
   })
@@ -26,7 +26,7 @@ describe("shouldRetry", () => {
   it("shouldn't retry messages too often that have already been retried", () => {
     const receivedDate = new Date()
     const message = mockAuditLog(receivedDate)
-    const event = mockAuditLogEvent("information", "Retrying failed message", new Date(Date.now() - 86399))
+    const event = mockAuditLogEvent("information", "Retrying failed message", new Date(Date.now() - 86399000))
     message.events.push(event)
     expect(shouldRetry(message)).toBeFalsy()
   })
@@ -34,9 +34,9 @@ describe("shouldRetry", () => {
   it("shouldn't retry messages too many times", () => {
     const receivedDate = new Date()
     const message = mockAuditLog(receivedDate)
-    const event1 = mockAuditLogEvent("information", "Retrying failed message", new Date(Date.now() - 186401))
-    const event2 = mockAuditLogEvent("information", "Retrying failed message", new Date(Date.now() - 286401))
-    const event3 = mockAuditLogEvent("information", "Retrying failed message", new Date(Date.now() - 386401))
+    const event1 = mockAuditLogEvent("information", "Retrying failed message", new Date(Date.now() - 186401000))
+    const event2 = mockAuditLogEvent("information", "Retrying failed message", new Date(Date.now() - 286401000))
+    const event3 = mockAuditLogEvent("information", "Retrying failed message", new Date(Date.now() - 386401000))
     message.events.push(event1)
     message.events.push(event2)
     message.events.push(event3)
