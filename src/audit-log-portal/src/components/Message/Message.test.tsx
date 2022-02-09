16e994852b9e86d6cd943145d56933cd93b1a6f9
Ben Pirt <ben@pirt.co.uk>
2022-02-09 13:47:43 +0000

    Fix snapshot error

--- src/audit-log-portal/src/components/Message/Message.test.tsx
+++ src/audit-log-portal/src/components/Message/Message.test.tsx
@@ -8,6 +8,8 @@ test("matches snapshot", () => {
   MockDate.set(new Date("2021-05-24T10:20:30").getTime())
 
   const message = new AuditLog("ExternalCorrelationId", new Date("2021-05-01T10:25:53"), "XML")
+  // @ts-ignore
+  message.messageId = "test-uuid"
 
   const { container } = render(<Message message={message} reloadMessages={() => {}} />)
 
