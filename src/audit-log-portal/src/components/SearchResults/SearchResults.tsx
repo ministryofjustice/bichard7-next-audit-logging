87d9c2bb059b31713ef53f21251dc4930469a894
Alice Lee <alice.lee@madetech.com>
2022-02-23 18:03:10 +0000

    validate UUIDs properly

--- src/audit-log-portal/src/components/SearchResults/SearchResults.tsx
+++ src/audit-log-portal/src/components/SearchResults/SearchResults.tsx
@@ -1,14 +1,14 @@
 import type MessageSearchModel from "types/MessageSearchModel"
 import GetMessageByIdResults from "./GetMessageByIdResults"
 import MessagesSearchResults from "./MessagesSearchResults"
+import { validate as validateUuid } from "uuid"
 
 export interface Props {
   searchModel: MessageSearchModel
 }
 
 const SearchResults = ({ searchModel }: Props) => {
-  const hexDigit = "\\da-fA-F"
-  if (searchModel.searchId?.match(/[\da-fA-F]{8}-[\da-fA-F]{4}-[\da-fA-F]{4}-[\da-fA-F]{4}-[\da-fA-F]{12}/)) {
+  if (validateUuid(searchModel.searchId)) {
     return <GetMessageByIdResults searchModel={searchModel} />
   } else {
     return <MessagesSearchResults searchModel={searchModel} />
