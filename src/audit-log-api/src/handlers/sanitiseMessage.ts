ab96447a417c52e9c0d47119539018984a627454
csabagyorfi <csaba@madetech.com>
2022-04-07 11:57:30 +0100

    Add sanitise messages api endpoint

---
+++ src/audit-log-api/src/handlers/sanitiseMessage.ts
@@ -0,0 +1,12 @@
+import type { APIGatewayProxyEvent, APIGatewayProxyResult } from "aws-lambda"
+import { HttpStatusCode } from "shared"
+import type { PromiseResult } from "shared-types"
+import { createJsonApiResult } from "../utils"
+
+/* eslint-disable require-await */
+export default async function sanitiseMessage(event: APIGatewayProxyEvent): PromiseResult<APIGatewayProxyResult> {
+  return createJsonApiResult({
+    statusCode: HttpStatusCode.noContent,
+    body: ""
+  })
+}
