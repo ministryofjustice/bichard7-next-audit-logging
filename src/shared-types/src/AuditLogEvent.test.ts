f7bb696c18f8245cf1ae4526eadf90c539a57225
Emad Karamad <emad.karamad@madetech.com>
2022-04-07 12:16:13 +0100

    Remove truncate logic from AuditLogEvent

--- src/shared-types/src/AuditLogEvent.test.ts
+++ src/shared-types/src/AuditLogEvent.test.ts
@@ -10,29 +10,12 @@ const defaultOptions: AuditLogEventOptions = {
 
 describe("AuditLogEvent", () => {
   describe("addAttribute", () => {
-    it("should truncate the attribute if the text is too long", () => {
-      const value = "x".repeat(2000)
+    it("should add the attribute key and value", () => {
+      const value = "x".repeat(10000)
       const log = new AuditLogEvent(defaultOptions)
       log.addAttribute("test", value)
       const attrValue: string = log.attributes.test as string
-      expect(attrValue).toHaveLength(1000)
-      expect(attrValue.endsWith("xxx...[truncated]")).toBeTruthy()
-    })
-
-    it("should not truncate the attribute if the text is not too long", () => {
-      const value = "x".repeat(1000)
-      const log = new AuditLogEvent(defaultOptions)
-      log.addAttribute("test", value)
-      const attrValue: string = log.attributes.test as string
-      expect(attrValue).toHaveLength(1000)
       expect(attrValue).toEqual(value)
-    })
-
-    it("should only try to truncate if the value is a string", () => {
-      const value = 999
-      const log = new AuditLogEvent(defaultOptions)
-      log.addAttribute("test", value)
-      expect(log.attributes.test).toEqual(value)
     })
   })
 })
