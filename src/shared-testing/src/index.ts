0b9139df91ddb67bbae879d798b64f8e90adf956
Emad Karamad <emad.karamad@madetech.com>
2022-04-07 12:16:35 +0100

    Add FakeAuditLogLookupDynamoGateway

--- src/shared-testing/src/index.ts
+++ src/shared-testing/src/index.ts
@@ -1,16 +1,10 @@
-// eslint-disable-next-line import/prefer-default-export
 export { default as FakeApiClient } from "./FakeApiClient"
-// eslint-disable-next-line import/prefer-default-export
 export { default as setEnvironmentVariables } from "./setEnvironmentVariables"
-// eslint-disable-next-line import/prefer-default-export
 export { default as FakeAuditLogDynamoGateway } from "./FakeAuditLogDynamoGateway"
-// eslint-disable-next-line import/prefer-default-export
+export { default as FakeAuditLogLookupDynamoGateway } from "./FakeAuditLogLookupDynamoGateway"
 export { default as invokeFunction } from "./invokeFunction"
-// eslint-disable-next-line import/prefer-default-export
 export { default as FakeMqGateway } from "./FakeMqGateway"
-// eslint-disable-next-line import/prefer-default-export
 export { default as FakeS3Gateway } from "./FakeS3Gateway"
-// eslint-disable-next-line import/prefer-default-export
 export { default as StepFunctionSimulator } from "./StepFunctionSimulator"
 export * from "./createMockAuditLogs"
 export * from "./mockAuditLogs"
