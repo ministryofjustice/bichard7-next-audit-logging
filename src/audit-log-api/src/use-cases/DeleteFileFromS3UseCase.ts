8fcbb2a05d5efe83575f880e57662f0eeda18274
Emad Karamad <emad.karamad@madetech.com>
2022-04-11 14:46:02 +0100

    Add DeleteMessageObjectsFromS3UseCase

--- src/audit-log-api/src/use-cases/DeleteFileFromS3UseCase.ts
+++
@@ -1,9 +0,0 @@
-import type { S3GatewayInterface, PromiseResult } from "shared-types"
-
-export default class DeleteFileFromS3UseCase {
-  constructor(private readonly gateway: S3GatewayInterface) {}
-
-  call(s3Path: string): PromiseResult<void> {
-    return this.gateway.deleteItem(s3Path)
-  }
-}
