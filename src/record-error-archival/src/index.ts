2be59cf1c7f11a03dd4c2b6efdb77b59bf09d36e
Alice Lee <alice.lee@madetech.com>
2022-04-21 15:29:49 +0100

    use module format expected by aws for lambdas

--- src/record-error-archival/src/index.ts
+++ src/record-error-archival/src/index.ts
@@ -4,29 +4,34 @@ import DatabaseClient from "./DatabaseClient"
 import { AuditLogApiClient } from "shared"
 import { logger } from "shared"
 
-const db = new DatabaseClient(
-  config.dbHost,
-  config.dbUser,
-  config.dbPassword,
-  config.dbSsl,
-  config.dbName,
-  config.dbSchema,
-  config.archiveGroupLimit
-)
-const auditLogApi = new AuditLogApiClient(config.apiUrl, config.apiKey)
+export default async function doRecordErrorArchival(): Promise<void> {
+  const db = new DatabaseClient(
+    config.dbHost,
+    config.dbUser,
+    config.dbPassword,
+    config.dbSsl,
+    config.dbName,
+    config.dbSchema,
+    config.archiveGroupLimit
+  )
+  const auditLogApi = new AuditLogApiClient(config.apiUrl, config.apiKey)
 
-recordErrorArchival(db, auditLogApi)
-  .catch((err) => {
-    db.disconnect()
-    throw err
-  })
-  .then((statuses) => {
+  try {
+    const statuses = await recordErrorArchival(db, auditLogApi)
     const successfulRecords = statuses?.filter((record) => record.success).length ?? 0
-    logger.info({ message: `Successfully updated ${successfulRecords} records`, successfulRecords: successfulRecords })
+    logger.info({
+      message: `Successfully updated ${successfulRecords} records`,
+      successfulRecords: successfulRecords
+    })
 
     const failedRecords = statuses?.filter((record) => !record.success)
 
     if (failedRecords !== undefined && failedRecords.length > 0) {
       logger.error({ message: "Failed to audit log some archived errors", failedRecords: failedRecords })
     }
-  })
+    return
+  } catch (error) {
+    db.disconnect()
+    throw error
+  }
+}
