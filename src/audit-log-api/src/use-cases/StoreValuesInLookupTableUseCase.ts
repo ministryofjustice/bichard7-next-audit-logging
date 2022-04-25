30eb229342d377f32e562b141f421b9719b0caec
Emad Karamad <emad.karamad@madetech.com>
2022-04-25 18:32:23 +0100

    Update StoreValuesInLookupTable use case for eventXml

--- src/audit-log-api/src/use-cases/StoreValuesInLookupTableUseCase.ts
+++ src/audit-log-api/src/use-cases/StoreValuesInLookupTableUseCase.ts
@@ -1,10 +1,17 @@
-import type { AuditLogEvent, AuditLogLookupDynamoGateway, KeyValuePair, PromiseResult } from "shared-types"
+import type {
+  AuditLogEvent,
+  AuditLogLookupDynamoGateway,
+  BichardAuditLogEvent,
+  KeyValuePair,
+  PromiseResult,
+  ValueLookup
+} from "shared-types"
 import { AuditLogLookup, isError } from "shared-types"
 
 const maxAttributeValueLength = 1000
 
 export default class StoreValuesInLookupTableUseCase {
-  constructor(private readonly lookupGateway: AuditLogLookupDynamoGateway) {}
+  constructor(private readonly lookupGateway: AuditLogLookupDynamoGateway) { }
 
   async execute(event: AuditLogEvent, messageId: string): PromiseResult<AuditLogEvent> {
     const attributes: KeyValuePair<string, unknown> = {}
@@ -20,15 +27,28 @@ export default class StoreValuesInLookupTableUseCase {
           return lookupItem
         }
 
-        attributes[attributeKey] = { valueLookup: lookupItem.id }
+        attributes[attributeKey] = { valueLookup: lookupItem.id } as ValueLookup
       } else {
         attributes[attributeKey] = attributeValue
       }
     }
 
+    let eventXml: string | undefined | ValueLookup =
+      "eventXml" in event ? (event as BichardAuditLogEvent).eventXml : undefined
+    if (eventXml) {
+      const lookupItem = await this.lookupGateway.create(new AuditLogLookup(eventXml, messageId))
+
+      if (isError(lookupItem)) {
+        return lookupItem
+      }
+
+      eventXml = { valueLookup: lookupItem.id }
+    }
+
     return {
       ...event,
-      attributes
+      attributes,
+      ...(eventXml ? { eventXml } : {})
     } as AuditLogEvent
   }
 }
