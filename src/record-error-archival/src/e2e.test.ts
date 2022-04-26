fbc8159b8d8145c65a264ce14c5a9f6a422a0f5e
Alice Lee <alice.lee@madetech.com>
2022-04-26 14:44:31 +0100

    invoke lambda in e2e tests

--- src/record-error-archival/src/e2e.test.ts
+++ src/record-error-archival/src/e2e.test.ts
@@ -1,7 +1,19 @@
+process.env.API_URL = "http://localhost:3010"
+process.env.API_KEY = "apiKey"
+process.env.DB_HOST = "localhost"
+process.env.DB_USER = "bichard"
+process.env.DB_PASSWORD = "password"
+process.env.DB_NAME = "bichard"
+
 import { Client } from "pg"
+import { AuditLogApiClient } from "shared"
+import type { ApiClient, AuditLog } from "shared-types"
+import { execute } from "lambda-local"
+import doRecordErrorArchival from "."
 
 describe("Record Error Archival e2e", () => {
   let pg: Client
+  let api: ApiClient
 
   beforeAll(async () => {
     pg = new Client({
@@ -15,11 +27,12 @@ describe("Record Error Archival e2e", () => {
 
     pg.connect()
 
-    await pg.query(`DROP SCHEMA br7own CASCADE`)
+    await pg.query(`DROP SCHEMA IF EXISTS br7own CASCADE`)
 
     const createTableSql = `
     CREATE SCHEMA br7own;
     GRANT ALL ON SCHEMA br7own TO bichard;
+
     CREATE TABLE br7own.archive_log
     (
         log_id          SERIAL PRIMARY KEY,
@@ -45,13 +58,20 @@ describe("Record Error Archival e2e", () => {
     await pg.query(
       `INSERT INTO br7own.archive_error_list (error_id, message_id, archive_log_id) VALUES (1, 'message_1', 1)`
     )
+
+    api = new AuditLogApiClient("http://localhost:3010", "apiKey")
+    await api.createAuditLog({
+      messageId: "message_1",
+      receivedDate: new Date().toISOString(),
+      events: []
+    } as unknown as AuditLog)
   })
 
   afterAll(async () => {
     await pg.end()
   })
 
-  it("should archive record errors successfully", () => {
+  it("should archive record errors successfully", async () => {
     /*
       Connect to postgres
       Ensure database is clear
@@ -67,6 +87,21 @@ describe("Record Error Archival e2e", () => {
       Ensure correct data in postgres
       Ensure any metrics are published
     */
+
+    await execute({
+      event: {},
+      lambdaFunc: { handler: doRecordErrorArchival },
+      region: "eu-west-2",
+      timeoutMs: 120 * 1_000,
+      environment: {
+        API_URL: "http://localhost:3010",
+        API_KEY: "apiKey",
+        DB_HOST: "localhost",
+        DB_USER: "bichard",
+        DB_PASSWORD: "password",
+        DB_NAME: "bichard"
+      }
+    })
 
     expect(true).toBeTruthy()
   })
