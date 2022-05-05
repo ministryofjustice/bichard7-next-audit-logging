ce8ec32aca2e04ebcafd2e42e331ebcb6cdb7340
Emad Karamad <emad.karamad@madetech.com>
2022-05-05 11:50:03 +0100

    Update add-archival-events api to use shared types

--- src/add-archival-events/src/api.ts
+++ src/add-archival-events/src/api.ts
@@ -1,16 +1,13 @@
 import { logger } from "shared"
 import type { ApiClient, AuditLog } from "shared-types"
-import { AuditLogEvent, isError } from "shared-types"
+import { AuditLogEvent, isError, EventType } from "shared-types"
 import type { BichardRecord } from "./db"
-
-const ARCHIVAL_EVENT_TYPE = "Error record archival"
-const ARCHIVAL_EVENT_CATAGORY = "information"
 
 const hasArchivalEvent = (auditLog: AuditLog, recordId: number): boolean =>
   auditLog.events.filter((event) => {
     return (
-      event.eventType === ARCHIVAL_EVENT_TYPE &&
-      event.category === ARCHIVAL_EVENT_CATAGORY &&
+      event.eventType === EventType.ErrorRecordArchival &&
+      event.category === "information" &&
       (event.attributes["Record ID"] || "") === recordId
     )
   }).length > 0
@@ -36,8 +33,8 @@ export const isRecordInAuditLog = async (
 export const createArchivalEventInAuditLog = async (api: ApiClient, bichardRecord: BichardRecord): Promise<boolean> => {
   const auditLogEvent = new AuditLogEvent({
     eventSource: bichardRecord.archivedBy,
-    category: ARCHIVAL_EVENT_CATAGORY,
-    eventType: ARCHIVAL_EVENT_TYPE,
+    category: "information",
+    eventType: EventType.ErrorRecordArchival,
     timestamp: bichardRecord.archivedAt
   })
   auditLogEvent.addAttribute("Record ID", bichardRecord.recordId)
