38e9fd5c1ac628c88effc9badbf12c3d5dd2fa82
Alice Lee <alice.lee@madetech.com>
2022-09-26 11:40:41 +0100

    Add more e2e tests for creating multiple audit log events
    Co-authored-by: Joe Folkard <joe.folkard@madetech.com>

--- src/audit-log-api/src/handlers/createAuditLogEvents.e2e.test.ts
+++ src/audit-log-api/src/handlers/createAuditLogEvents.e2e.test.ts
@@ -19,7 +19,7 @@ describe("Creating Audit Log events", () => {
     await gateway.deleteAll(dynamoConfig.TABLE_NAME, "messageId")
   })
 
-  it("should create a new audit log event for an existing audit log record", async () => {
+  it("should create a single new audit log event for an existing audit log record", async () => {
     const auditLog = mockAuditLog()
     const result1 = await axios.post("http://localhost:3010/messages", auditLog)
     expect(result1.status).toEqual(HttpStatusCode.created)
@@ -43,5 +43,32 @@ describe("Creating Audit Log events", () => {
     actualEvent.attributes["Attribute 1"] = event.attributes["Attribute 1"]
     expect(actualEvent.eventXml).toHaveProperty("valueLookup")
     expect({ ...actualEvent, eventXml: undefined }).toEqual({ ...event, eventXml: undefined })
+  })
+
+  it("should create many new audit log events for an existing audit log record", async () => {
+    const auditLog = mockAuditLog()
+    const result1 = await axios.post("http://localhost:3010/messages", auditLog)
+    expect(result1.status).toEqual(HttpStatusCode.created)
+
+    const events = new Array(10).fill(0).map(() => mockAuditLogEvent())
+    const result2 = await axios.post(`http://localhost:3010/messages/${auditLog.messageId}/manyEvents`, events)
+    expect(result2.status).toEqual(HttpStatusCode.created)
+
+    const record = await gateway.getOne<AuditLog>(dynamoConfig.TABLE_NAME, "messageId", auditLog.messageId)
+
+    expect(record).not.toBeNull()
+
+    const { messageId, events: dynamoEvents } = record!
+    expect(messageId).toEqual(auditLog.messageId)
+
+    expect(dynamoEvents).toHaveLength(10)
+    dynamoEvents.forEach((event) => {
+      const actualEvent = event as BichardAuditLogEvent
+      expect(actualEvent.attributes?.["Attribute 1"]).toHaveProperty("valueLookup")
+
+      actualEvent.attributes["Attribute 1"] = event.attributes["Attribute 1"]
+      expect(actualEvent.eventXml).toHaveProperty("valueLookup")
+      expect({ ...actualEvent, eventXml: undefined }).toEqual({ ...event, eventXml: undefined })
+    })
   })
 })
--- src/audit-log-api/src/handlers/createAuditLogEvents.test.ts
+++ src/audit-log-api/src/handlers/createAuditLogEvents.test.ts
@@ -5,18 +5,27 @@ import { HttpStatusCode } from "shared"
 import { CreateAuditLogEventsUseCase } from "../use-cases"
 import createAuditLogEvents from "./createAuditLogEvents"
 
-const createHandlerEvent = (auditLogEvent?: AuditLogEvent): APIGatewayProxyEvent => {
-  const event =
-    auditLogEvent ||
+const createTestAuditLogEvent = (): AuditLogEvent => {
+  return new AuditLogEvent({
+    category: "information",
+    timestamp: new Date(),
+    eventType: "Test event",
+    eventSource: "Test"
+  })
+}
+
+const createHandlerEvent = (...auditLogEvents: AuditLogEvent[]): APIGatewayProxyEvent => {
+  const events = auditLogEvents || [
     new AuditLogEvent({
       category: "information",
       timestamp: new Date(),
       eventType: "Test event",
       eventSource: "Test"
     })
+  ]
 
   return <APIGatewayProxyEvent>{
-    body: JSON.stringify([event]),
+    body: JSON.stringify(events),
     pathParameters: <unknown>{
       messageId: "1f5a462b-781d-4805-bbcb-51c2c0d1a8ee" // Random messageId
     }
@@ -38,9 +47,36 @@ describe("createAuditLogEvents()", () => {
     expect(actualResponse.body).toBe("Created")
   })
 
-  it("should return Bad request status when audit log event is not valid", async () => {
+  it("should return Created status when many events are added to the audit log in the database", async () => {
+    jest.spyOn(CreateAuditLogEventsUseCase.prototype, "create").mockReturnValue(
+      Promise.resolve({
+        resultType: "success"
+      })
+    )
+
+    const auditLogEvents = new Array(10).fill(0).map(() => createTestAuditLogEvent())
+    const event = createHandlerEvent(...auditLogEvents)
+    const actualResponse = await createAuditLogEvents(event)
+
+    expect(actualResponse.statusCode).toBe(HttpStatusCode.created)
+    expect(actualResponse.body).toBe("Created")
+  })
+
+  it("should return Bad request status when a single audit log event is not valid", async () => {
     const event = createHandlerEvent({} as AuditLogEvent)
     const actualResponse = await createAuditLogEvents(event)
+
+    expect(actualResponse.statusCode).toBe(HttpStatusCode.badRequest)
+    expect(actualResponse.body).toBe(
+      "Category is mandatory, Event source is mandatory, Event type is mandatory, Timestamp is mandatory"
+    )
+  })
+
+  it("should return Bad request status when a one audit log event among many is not valid", async () => {
+    const events = new Array(10).fill(0).map(() => createTestAuditLogEvent())
+    events.splice(4, 0, {} as AuditLogEvent)
+    const handlerEvent = createHandlerEvent(...events)
+    const actualResponse = await createAuditLogEvents(handlerEvent)
 
     expect(actualResponse.statusCode).toBe(HttpStatusCode.badRequest)
     expect(actualResponse.body).toBe(
@@ -80,7 +116,7 @@ describe("createAuditLogEvents()", () => {
     expect(actualResponse.body).toBe(expectedMessage)
   })
 
-  it("should respond with a Conflict status when an the message version is different", async () => {
+  it("should respond with a Conflict status when the message version is different", async () => {
     const expectedMessage = "Expected Message"
     jest.spyOn(CreateAuditLogEventsUseCase.prototype, "create").mockReturnValue(
       Promise.resolve({
