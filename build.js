46951309acdc8ad32c62da2fcc98383dd7074d23
Jamie Davies <jamie.davies@madetech.com>
2022-11-17 14:32:24 +0000

    Remove getEvents (GET .../events) lambda
    The events are already included in the message when calling
    /message/<id>, so having a separate endpoint just to get the events is
    unnecessary.

--- build.js
+++ build.js
@@ -5,7 +5,6 @@ require("esbuild")
       "src/archive-user-logs/index.ts",
       "src/audit-log-api/handlers/createAuditLog.ts",
       "src/audit-log-api/handlers/createAuditLogEvents.ts",
-      "src/audit-log-api/handlers/getEvents.ts",
       "src/audit-log-api/handlers/getMessages.ts",
       "src/audit-log-api/handlers/retryMessage.ts",
       "src/audit-log-api/handlers/sanitiseMessage.ts",
