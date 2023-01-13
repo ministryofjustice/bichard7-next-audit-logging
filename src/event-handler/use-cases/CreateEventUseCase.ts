a8a81a4565a255904de93dc48be112c200ccbc3b
Emad Karamad <emad.karamad@madetech.com>
2023-01-13 14:17:37 +0000

    Refactor CreateEventUseCase (#422)

--- src/event-handler/use-cases/CreateEventUseCase.ts
+++ src/event-handler/use-cases/CreateEventUseCase.ts
@@ -5,16 +5,16 @@ export default class {
   constructor(private readonly api: ApiClient) {}
 
   execute(messageId: string, event: ApiAuditLogEvent): PromiseResult<void> {
-    if (!messageId) {
-      const userName = event.attributes?.["User ID"]
-      if (userName) {
-        return this.api.createUserEvent(String(userName), event)
-      }
-
-      logger.info(`No messageId or userName: ${JSON.stringify(event)}`)
-      return Promise.resolve(undefined)
+    if (messageId) {
+      return this.api.createEvent(messageId, event)
     }
 
-    return this.api.createEvent(messageId, event)
+    const userName = event.attributes?.["User ID"]
+    if (userName) {
+      return this.api.createUserEvent(String(userName), event)
+    }
+
+    logger.info(`No messageId or userName: ${JSON.stringify(event)}`)
+    return Promise.resolve(undefined)
   }
 }
