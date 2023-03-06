a590fb3c5e6ed39916300f66df76e1045e657b69
Emad Karamad <emad.karamad@madetech.com>
2023-03-06 10:36:47 +0000

    Fix issue with installing dynamodb local

---
+++ scripts/install-dynamodb-local.sh
@@ -0,0 +1,6 @@
+if [ ! -d ./.dynamodb ]; then
+  curl https://s3.eu-central-1.amazonaws.com/dynamodb-local-frankfurt/dynamodb_local_latest.tar.gz --output dynamodb.tar.gz
+  mkdir -p ./.dynamodb
+  tar -xf dynamodb.tar.gz -C ./.dynamodb
+  rm dynamodb.tar.gz
+fi
