93f73cf6f9e30f7e2a529e08b153873f5385cefc
Tausif Patel <tausifpatel.dev@gmail.com>
2025-08-22 11:48:42 +0100

    Update config to fetch API_KEY and DB_PASSWORD from SSM

--- src/add-archival-events/config.ts
+++ src/add-archival-events/config.ts
@@ -1,9 +1,31 @@
-export default () => {
+import type { SSM } from "aws-sdk"
+import { isError } from "src/shared/types"
+
+const getSSMParameter = async (ssm: SSM, paramName: string, paramDescription: string): Promise<string> => {
+  const result = await ssm
+    .getParameter({ Name: paramName, WithDecryption: true })
+    .promise()
+    .catch((error: Error) => error)
+
+  if (isError(result)) {
+    throw result
+  }
+
+  const value = result.Parameter?.Value
+
+  if (!value) {
+    throw Error(`Could't retrieve ${paramDescription} from SSM`)
+  }
+
+  return value
+}
+
+export default async (ssm: SSM) => {
   if (!process.env.API_URL) {
     throw new Error("API_URL environment variable is not set")
   }
-  if (!process.env.API_KEY) {
-    throw new Error("API_KEY environment variable is not set")
+  if (!process.env.API_KEY_ARN) {
+    throw new Error("API_KEY_ARN environment variable is not set")
   }
   if (!process.env.DB_HOST) {
     throw new Error("DB_HOST environment variable is not set")
@@ -11,20 +33,23 @@ export default () => {
   if (!process.env.DB_USER) {
     throw new Error("DB_USER environment variable is not set")
   }
-  if (!process.env.DB_PASSWORD) {
-    throw new Error("DB_PASSWORD environment variable is not set")
+  if (!process.env.DB_PASSWORD_ARN) {
+    throw new Error("DB_PASSWORD_ARN environment variable is not set")
   }
   if (!process.env.DB_NAME) {
     throw new Error("DB_NAME environment variable is not set")
   }
 
+  const apiKey = await getSSMParameter(ssm, process.env.API_KEY_ARN, "API key")
+  const dbPassword = await getSSMParameter(ssm, process.env.DB_PASSWORD_ARN, "DB password")
+
   return {
     apiUrl: process.env.API_URL,
-    apiKey: process.env.API_KEY,
+    apiKey,
     dbHost: process.env.DB_HOST,
     dbPort: process.env.DB_PORT ? Number(process.env.DB_PORT) : 5432,
     dbUser: process.env.DB_USER,
-    dbPassword: process.env.DB_PASSWORD,
+    dbPassword,
     dbSsl: !!process.env.DB_SSL,
     dbName: process.env.DB_NAME,
     dbSchema: process.env.DB_SCHEMA || "br7own",
